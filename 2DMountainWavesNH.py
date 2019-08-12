Fixed terrain processing FFT interpolation. No TF x derivative.

2DMountainWavesNH.py
@@ -43,9 +43,9 @@ from computeNeumannAdjusted import computeNeumannAdjusted
 
 if __name__ == '__main__':
        # Set the solution type
-       StaticSolve = True
-       TransientSolve = False
-       DynSGS = False
+       StaticSolve = False
+       TransientSolve = True
+       DynSGS = True
        
        # Set physical constants (dry air)
        gc = 9.80601
@@ -73,7 +73,7 @@ if __name__ == '__main__':
        OPS = NX * NZ
        
        # Set the terrain options
-       h0 = 10.0
+       h0 = 100.0
        aC = 5000.0
        lC = 4000.0
        HOPT = [h0, aC, lC]
@@ -88,7 +88,7 @@ if __name__ == '__main__':
        # Define the computational and physical grids+
        REFS = computeGrid(DIMS)
        
-       #%% Compute the raw derivative matrix operators in alpha-xi computational space
+       #% Compute the raw derivative matrix operators in alpha-xi computational space
        DDX_1D, HF_TRANS = computeHermiteFunctionDerivativeMatrix(DIMS)
        DDZ_1D, CH_TRANS = computeChebyshevDerivativeMatrix(DIMS)
        
@@ -100,8 +100,8 @@ if __name__ == '__main__':
        DDX_1D_NEUMANN = DDX_1D
        DDZ_1D_NEUMANN = computeNeumannAdjusted(DDZ_1D, True, True)
        
-       #%% Read in topography profile or compute from analytical function
-       AGNESI = 1 # "Witch of Agnesi" profile
+       #% Read in topography profile or compute from analytical function
+       AGNESI = 1 # "Witch of Agnesi" profil e
        SCHAR = 2 # Schar mountain profile nominal (Schar, 2001)
        EXPCOS = 3 # Even exponential and squared cosines product
        EXPPOL = 4 # Even exponential and even polynomial product
@@ -109,7 +109,7 @@ if __name__ == '__main__':
        HofX, dHdX = computeTopographyOnGrid(REFS, SCHAR, HOPT)
        
        # Compute the terrain derivatives by Hermite-Function derivative matrix
-       dHdX = DDX_1D.dot(HofX)
+       #dHdX = DDX_1D.dot(HofX)
        
        # Make the 2D physical domains from reference grids and topography
        XL, ZTL, DZT, sigma = computeGuellrichDomain2D(DIMS, REFS, HofX, dHdX)
@@ -251,7 +251,7 @@ if __name__ == '__main__':
               #'''
               # Transient solve parameters
               DT = 0.05
-              HR = 5.0
+              HR = 1.0
               ET = HR * 60 * 60 # End time in seconds
               TI = np.array(np.arange(DT, ET, DT))
               OTI = 50 # Stride for diagnostic output
@@ -415,11 +415,11 @@ if __name__ == '__main__':
        
        #%%''' #Spot check the solution on both grids
        fig = plt.figure()
-       ccheck = plt.contourf(XL, ZTL, wxz, 101, cmap=cm.seismic)
+       ccheck = plt.contourf(XL, ZTL, uxz, 101, cmap=cm.seismic)
        cbar = fig.colorbar(ccheck)
        #
        fig = plt.figure()
-       ccheck = plt.contourf(XLI, ZTLI, wxzint, 101, cmap=cm.seismic)
+       ccheck = plt.contourf(XLI, ZTLI, uxzint, 101, cmap=cm.seismic)
        cbar = fig.colorbar(ccheck)
        #
        fig = plt.figure()

computeTopographyOnGrid.py
@@ -5,16 +5,21 @@ Created on Fri Jul 19 14:43:05 2019
 
 @author: TempestGuerra
 """
-import sys, getopt
+import sys
 import numpy as np
 from numpy import multiply as mul
-from scipy import linalg as las
 import math as mt
+import matplotlib.pyplot as plt 
 
 def computeTopographyOnGrid(REFS, profile, opt):
        
        # Get data from REFS
-       x = REFS[0]
+       xh = REFS[0]
+       l2 = np.amax(xh)
+       l1 = np.amin(xh)
+       DX = 50.0 # maximum resolution...
+       NP = int((l2 - l1) / DX)
+       x = np.linspace(l1, l2, num=NP, endpoint=False)
        
        # Evaluate the function with different options
        if profile == 1:
@@ -38,23 +43,52 @@ def computeTopographyOnGrid(REFS, profile, opt):
               dhdx = mul(dht2, dht3)
               dhdx = np.add(dhdx, dht4)
               dhdx = mul(dht1, dhdx)
-              #dhdx = -DS.hC * exp(-xlon.^2/DS.aC^2) .* ( ...
-              #2.0 * xlon / (DS.aC^2) .* (cos(pi * xlon / DS.lC)).^2 + ...
-              #pi/DS.lC * sin(2.0 * pi * xlon / DS.lC));
        elif profile == 3:
               # General even power exponential times a cosine series
               h0 = opt[0]
+              ht = np.zeros(len(x))
        elif profile == 4:
               # General even power exponential times a polynomial series
               h0 = opt[0]
+              ht = np.zeros(len(x))
        elif profile == 5:
               # Terrain data input from a file, maximum elevation set in opt[0]
               h0 = opt[0]
+              ht = np.zeros(len(x))
        else:
               print('ERROR: invalid terrain option.')
               sys.exit(2)
               
-       # Check that terrain data is positive definite...
+       # Compute derivative by FFT
+       #Linp = 0.5 * (l2 - l1)
+       if NP % 2 == 0:
+              posRange = list(range(0, int(NP / 2)))
+              negRange = list(range(-int(NP / 2 + 1), 0))
+              k = np.array(posRange + [0] + negRange, dtype=np.float64)
+       else:
+              posRange = list(range(0, int((NP - 1) / 2)))
+              negRange = list(range(-int((NP - 1) / 2), 0))
+              k = np.array(posRange + [0] + negRange, dtype=np.float64)
+
+       # Scale the frequency array
+       ks = 2 * np.pi / (l2 - l1) * k
+       # Compute derivative by FFT
+       HF = np.fft.fft(ht)
+       DHDX = 1j * np.multiply(ks, HF)
+       # Compute the orthogonal projection to the xh grid
+       FIM = 1j * np.zeros((len(xh), NP))
+       # Shift domain to positive
+       xh += l2
+       # Compute the Fourier basis on the desired grid
+       for cc in range(len(k)):
+              arg = 1j * ks[cc] * xh
+              FIM[:,cc] = 1.0 / NP * np.exp(arg)
+       xh -= l2
+              
+       # Compute the inverse Fourier interpolation
+       ht = np.dot(FIM, HF)
+       dhdx = np.dot(FIM, DHDX)
+       plt.plot(xh, np.real(dhdx))
        
-       return ht, dhdx
+       return np.real(ht), np.real(dhdx)
               