Trying to fix oscillation problem in HF derivative...

computeHermiteFunctionDerivativeMatrix.py
@@ -19,8 +19,10 @@ def computeHermiteFunctionDerivativeMatrix(DIMS):
        NX = DIMS[3]
        
        alpha, whf = hefunclb(NX)
-       HT = hefuncm(NX-1, alpha, True)
-       HTD = hefuncm(NX, alpha, True)
+       HT = hefuncm(NX, alpha, True)
+       HTD = hefuncm(NX+1, alpha, True)
+       print(HT.shape)
+       print(HTD.shape)
        
        # Get the scale factor
        b = (np.amax(alpha) - np.min(alpha)) / abs(L2 - L1)
@@ -29,19 +31,19 @@ def computeHermiteFunctionDerivativeMatrix(DIMS):
        W = np.diag(whf, k=0)
        
        # Compute the coefficients of spectral derivative in matrix form
-       SDIFF = np.zeros((NX+1,NX));
-       SDIFF[0,1] = mt.sqrt(0.5);
+       SDIFF = np.zeros((NX+2,NX+1));
+       SDIFF[NX+1,NX] = -mt.sqrt((NX + 1) * 0.5);
        SDIFF[NX,NX-1] = -mt.sqrt(NX * 0.5);
-       SDIFF[NX-1,NX-2] = -mt.sqrt((NX - 1) * 0.5);
 
-       for cc in range(NX-3,-1,-1):
-              SDIFF[cc+1,cc+2] = mt.sqrt((cc + 2) * 0.5);
-              SDIFF[cc+1,cc] = -mt.sqrt((cc + 1) * 0.5);
+       for rr in range(NX-1,-1,-1):
+              SDIFF[rr,rr+1] = mt.sqrt((rr + 1) * 0.5);
+              if rr > 0:
+                     SDIFF[rr,rr-1] = -mt.sqrt(rr * 0.5);
               
        # Hermite function spectral transform in matrix form
-       STR_H = (HT).dot(W);
+       STR_H = (HT.T).dot(W);
        # Hermite function spatial derivative based on spectral differentiation
-       temp = (HTD.T).dot(SDIFF)
+       temp = (HTD).dot(SDIFF)
        temp = temp.dot(STR_H)
        DDM = b * temp
        