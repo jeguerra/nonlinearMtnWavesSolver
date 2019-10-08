d7d2887a000a2680be31b2edb52e154577740cdf
jorge guerra <jorge.guerra@noaa.gov>
2019-10-08 19:45:14 +0000

    Fixes to application of terrain following stretching.

--- computePartialDerivativesXZ.py
+++ computePartialDerivativesXZ.py
@@ -9,13 +9,15 @@ Created on Fri Aug  2 17:10:18 2019
 import numpy as np
 import scipy.sparse as sps
 
-def computePartialDerivativesXZ(DIMS, REFS, DDX_1D, DDZ_1D):
+def computePartialDerivativesXZ(DIMS, REFS):
        # Get the dimensions
        NX = DIMS[3] + 1
        NZ = DIMS[4]
        OPS = NX * NZ
        
        # Get REFS data
+       DDX_1D = REFS[2]
+       DDZ_1D = REFS[3]
        sigma = REFS[7]
        
        # Unwrap the 1D derivative matrices into 2D operators
@@ -25,7 +27,11 @@ def computePartialDerivativesXZ(DIMS, REFS, DDX_1D, DDZ_1D):
        for cc in range(NX):
               ddex = np.array(range(NZ)) + cc * NZ
               # Advanced slicing used to get submatrix
-              DDZ_OP[np.ix_(ddex,ddex)] = DDZ_1D
+              # TF adjustment for Guellrich coordinate
+              #SIGMA = sps.diags(sigma[:,cc])
+              #DDZ_OP[np.ix_(ddex,ddex)] = SIGMA.dot(DDZ_1D)
+              # TF adjustment for vanilla stretching
+              DDZ_OP[np.ix_(ddex,ddex)] = sigma[cc] * DDZ_1D
        
        
        # Horizontal Derivative
@@ -33,15 +39,10 @@ def computePartialDerivativesXZ(DIMS, REFS, DDX_1D, DDZ_1D):
        for rr in range(NZ):
               ddex = np.array(range(0,OPS,NZ)) + rr
               # Advanced slicing used to get submatrix
-              #DDX_OP[np.ix_(ddex,ddex)] = sigma[cc] * DDX_1D
               DDX_OP[np.ix_(ddex,ddex)] = DDX_1D
               
        #%% Make the operators sparse
        DDXM = sps.csc_matrix(DDX_OP)
        DDZM = sps.csc_matrix(DDZ_OP)
-       
-       # Apply terrain following adjustments (Terrain metric)
-       SIGMA = sps.diags(np.reshape(sigma, (OPS,), order='F'), format='csc')       
-       DDZM = SIGMA.dot(DDZM)
        
        return DDXM, DDZM
