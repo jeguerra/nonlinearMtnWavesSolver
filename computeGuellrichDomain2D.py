354109e9340ba4b60060432d9c729a95708c6864
Jorge E Guerra <jeguerra@ucdavis.edu>
2019-10-08 13:26:58 -0600

    Fixes to the Guellrich coordinate.

--- computeGuellrichDomain2D.py
+++ computeGuellrichDomain2D.py
@@ -7,8 +7,8 @@ Created on Fri Jul 19 10:23:58 2019
 """
 
 import numpy as np
-from numpy import multiply as mul
 import math as mt
+#import matplotlib.pyplot as plt
 
 def computeGuellrichDomain2D(DIMS, REFS, hx, dhdx):
        # Get data from DIMS and REFS
@@ -21,37 +21,39 @@ def computeGuellrichDomain2D(DIMS, REFS, hx, dhdx):
        z = REFS[1]
        
        # Compute the flat XZ mesh
-       HTZL, dummy = np.meshgrid(hx,z);
-       XL, ZL = np.meshgrid(x,z);
+       HTZL, dummy = np.meshgrid(hx,z)
+       XL, ZL = np.meshgrid(x,z)
        
        # High Order Improved Guellrich coordinate 3 parameter function
-       xi = 1.0 / ZH * ZL;
-       ang = 0.5 * mt.pi * xi;
-       AR = 1.0E-3;
-       p = 20;
-       q = 5;
+       xi = 1.0 / ZH * ZL
+       ang = 0.5 * mt.pi * xi
+       AR = 1.0E-3
+       p = 20
+       q = 5
        
        expdec = np.exp(-p/q * xi)
        cosvar = np.power(np.cos(ang), p)
        cosvard = np.power(np.cos(ang), p-1)
-       fxi1 = mul(expdec, cosvar)
-       fxi2 = AR * mul(xi, (1.0 - xi));
+       fxi1 = expdec * cosvar
+       fxi2 = AR * (xi * (1.0 - xi));
        dzdh = np.add(fxi1, fxi2)
        
-       dfdxi1 = -p/q * mul(expdec, cosvar)
-       dfdxi2 = -(0.5 * p) * mt.pi * mul(mul(expdec, np.sin(ang)), cosvard)
+       dfdxi1 = -p * (expdec * cosvard)
+       dfdxi2 = (1.0/q * np.cos(ang) + 0.5 * mt.pi * np.sin(ang))
        dfdxi3 = -AR * (1.0 - 2.0 * xi);
-       d_dzdh_dxi = np.add(np.add(dfdxi1, dfdxi2), dfdxi3)
+       d_dzdh_dxi = (dfdxi1 * dfdxi2) + dfdxi3
        
-       dxidz = ZH + mul(HTZL, d_dzdh_dxi)
-       sigma = ZH * np.power(dxidz, -1.0)
+       dxidz = ZH + (HTZL * d_dzdh_dxi)
+       sigma = ZH * np.reciprocal(dxidz)
        
        # Make the global array of terrain height and slope features
        ZTL = np.zeros((NZ,NX))
        DZT = np.zeros((NZ,NX))
        
        for rr in range(NZ):
-              ZTL[rr,:] = np.add(mul(dzdh[rr,:], hx), ZL[rr,:])
-              DZT[rr,:] = mul(dzdh[rr,:], dhdx);
+              ZTL[rr,:] = (dzdh[rr,0] * hx) + ZL[rr,:]
+              DZT[rr,:] = dzdh[rr,0] * dhdx
+              
+       #plt.plot(z, dzdh[:,0])
        
        return XL, ZTL, DZT, sigma
