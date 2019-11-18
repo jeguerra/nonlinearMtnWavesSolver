Fixes and testing Jacobian solution.

2DMountainWavesNH.py
@@ -74,9 +74,9 @@ def getFromRestart(name, ET, NX, NZ):
        
 if __name__ == '__main__':
        # Set the solution type (MUTUALLY EXCLUSIVE)
-       StaticSolve = False
+       StaticSolve = True
        LinearSolve = False
-       NonLinSolve = True
+       NonLinSolve = False
        
        # Set residual diffusion switch
        ResDiff = True
@@ -87,7 +87,7 @@ if __name__ == '__main__':
        
        # Set restarting
        toRestart = True
-       isRestart = True
+       isRestart = False
        restart_file = 'restartDB'
        
        # Set physical constants (dry air)
@@ -320,8 +320,7 @@ if __name__ == '__main__':
               # Initialize time array
               TI = np.array(np.arange(DT, ET, DT))
               # Initialize fields
-              fields, U, RdT = eqs.computePrepareFields(PHYS, REFS, SOLT[:,0], INIT, udex, wdex, pdex, tdex, ubdex, utdex)
-              #fields, U, RdT = eqs.computeInitialFields(PHYS, REFS, SOLT[:,0], INIT, udex, wdex, pdex, tdex, ubdex, utdex)
+              fields, U, RdT = eqs.computeInitialFields(PHYS, REFS, SOLT[:,0], INIT, udex, wdex, pdex, tdex, ubdex, utdex)
               # Initialize the RHS and forcing for each field
               RHS = eqs.computeEulerEquationsLogPLogT_NL(PHYS, REFS, REFG, fields, U, RdT, ubdex, utdex)
        
@@ -334,8 +333,7 @@ if __name__ == '__main__':
               
               # Test evaluation of full Jacobian... must match linearization on first iteration
               if StaticSolve:
-                     fields, U, RdT = eqs.computeInitialFields(PHYS, REFS, SOLT[:,0], INIT, udex, wdex, pdex, tdex, ubdex, utdex)
-                     DOPS_NL = eqs.computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, ubdex, utdex, SOLT[:,0])
+                     DOPS_NL = eqs.computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, ubdex, utdex)
                      DOPS = [DOPS_NL[0], DOPS_NL[1], DOPS_NL[2], \
                              DOPS_NL[5], DOPS_NL[6], DOPS_NL[7], \
                              DOPS_NL[8], DOPS_NL[9], DOPS_NL[10], \
@@ -376,22 +374,20 @@ if __name__ == '__main__':
                      # No coupled BC implementation on restart
                      UW = None
                      UT = None
+                     
+                     # Compute the current boundary forcing
+                     WBC = dHdX * fields[ubdex,0]
               else:
                      # Set the forcing
                      bN = RHS
                      # No coupled BC implementation on restart
                      UW = None
                      UT = None
-                     '''
-                     # Compute the forcing
-                     WBC = dHdX * UZ[0,:]
-                     WEQ = sps.bmat([[((DOPS[1])[:,ubdex])], \
-                                     [((DOPS[3])[:,ubdex])], \
-                                     [((DOPS[7])[:,ubdex])], \
-                                     [((DOPS[9])[:,ubdex])]])
-                     bN = -WEQ.dot(WBC);
-                     del(WEQ)
                      
+                     # Compute the initial boundary forcing
+                     WBC = dHdX * U[ubdex]
+
+                     '''
                      # Compute coupled BC adjustment per block from w to u eqs.
                      WBC = sps.spdiags(dHdX, 0, len(dHdX), len(dHdX))
                      U_cols11 = ((DOPS[1].tolil())[:,ubdex]).dot(WBC)
@@ -415,7 +411,16 @@ if __name__ == '__main__':
                      del(U_cols11); del(U_cols21); del(U_cols31); del(U_cols41)
                      '''
               
+              # Apply terrain forcing
+              WEQ = sps.bmat([[((DOPS[1])[:,ubdex])], \
+                              [((DOPS[3])[:,ubdex])], \
+                              [((DOPS[7])[:,ubdex])], \
+                              [((DOPS[9])[:,ubdex])]])
+              bN -= WEQ.dot(WBC);
               del(DOPS)
+              del(WEQ)
+              
+              # Set up Schur blocks or full operator...
               if (StaticSolve and SolveSchur) and not LinearSolve:
                      # Compute the partitions for Schur Complement solution
                      AS = sps.bmat([[A + R1, B], [UW, D + R2]], format='csc')
@@ -509,9 +514,6 @@ if __name__ == '__main__':
                             f1 = np.concatenate((RHS[udex], fw[wbcDex]))
                             ft = RHS[tdex]
                             f2 = np.concatenate((RHS[pdex], ft[tbcDex]))
-                            
-                            #del(fw)
-                            #del(ft)
                      
                      # Get memory back
                      del(BS); del(CS)
@@ -532,7 +534,8 @@ if __name__ == '__main__':
               fields, U, RdT = eqs.computePrepareFields(PHYS, REFS, SOLT[:,1], INIT, udex, wdex, pdex, tdex, ubdex, utdex)
               RHS = eqs.computeEulerEquationsLogPLogT_NL(PHYS, REFS, REFG, fields, U, RdT, ubdex, utdex)
               print('Residual 2-norm after NL solve: ', np.linalg.norm(RHS))
-              
+       
+       #%% Transient solutions       
        elif LinearSolve:
               RHS[sysDex] = bN
               print('Starting Linear Transient Solver...')

computeEulerEquationsLogPLogT.py
@@ -27,11 +27,11 @@ def computeInitialFields(PHYS, REFS, SOLT, INIT, udex, wdex, pdex, tdex, botdex,
        fields = np.reshape(SOLT, (len(udex), 4), order='F')
        
        # The initial condition is an atmosphere in hydrostatic rest
-       DZDX = REFS[15]
+       #DZDX = REFS[15]
        # This sets the free slip condition on all coordinate surfaces
        # in the column. Ensures proper initialization of perturbation
        # fields. 
-       fields[:,1] = U * DZDX
+       #fields[:,1] = U * DZDX
 
        return fields, U, RdT
 
@@ -42,6 +42,7 @@ def computePrepareFields(PHYS, REFS, SOLT, INIT, udex, wdex, pdex, tdex, botdex,
        kap = PHYS[4]
        
        dHdX = REFS[6]
+       DZDX = REFS[15]
        
        TQ = SOLT + INIT
        # Make the total quatities
@@ -53,14 +54,19 @@ def computePrepareFields(PHYS, REFS, SOLT, INIT, udex, wdex, pdex, tdex, botdex,
        RdT = Rd * P0**(-kap) * np.exp(LT + kap * LP)
        
        fields = np.reshape(SOLT, (len(udex), 4), order='F')
+       
+       # SET FREE SLIP ON BOTTOM TERRAIN SURFACE
        fields[botdex,1] = U[botdex] * dHdX
        fields[topdex,1] *= 0.0
+       
+       # SET FREE SLIP ON ALL COORDINATE SURFACES
+       #fields[:,1] = U * DZDX
        fields[topdex,3] *= 0.0
 
        return fields, U, RdT
 
 #%% Evaluate the Jacobian matrix
-def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topdex, DQ):
+def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topdex):
        # Get physical constants
        gc = PHYS[0]
        Rd = PHYS[3]
@@ -154,6 +160,26 @@ def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topd
                LD41, LD42, LD43, LD44]
        
        return DOPS
+
+def computeJacobianVectorProduct(DOPS, REFG, vec, udex, wdex, pdex, tdex):
+       # Get the Rayleight operators
+       ROPS = REFG[5]
+       
+       # Compute the variable sections
+       uvec = vec[udex]
+       wvec = vec[wdex]
+       pvec = vec[pdex]
+       tvec = vec[tdex]
+       
+       # Compute the block products
+       ures = (DOPS[0] + ROPS[0]).dot(uvec) + DOPS[1].dot(wvec) + DOPS[2].dot(pvec) + DOPS[3].dot(tvec)
+       wres = DOPS[4].dot(uvec) + (DOPS[5] + ROPS[1]).dot(wvec) + DOPS[6].dot(pvec) + DOPS[7].dot(tvec)
+       pres = DOPS[8].dot(uvec) + DOPS[9].dot(wvec) + (DOPS[10] + ROPS[2]).dot(pvec)
+       tres = DOPS[12].dot(uvec) + DOPS[13].dot(wvec) + (DOPS[15] + ROPS[3]).dot(tvec)
+       
+       qprod = np.concatenate((ures, wres, pres, tres))
+       
+       return -qprod
     
 #%% The linear equation operator
 def computeEulerEquationsLogPLogT(DIMS, PHYS, REFS, REFG):
@@ -282,9 +308,9 @@ def computeEulerEquationsLogPLogT_NL(PHYS, REFS, REFG, fields, U, RdT, botdex, t
                      
        return np.concatenate(DqDt())
 
-def computeRayleighTendency(REFG, fields, udex, wdex, pdex, tdex, botdex, topdex):
+def computeRayleighTendency(REFG, fields, botdex, topdex):
        
-       # Get the static vertical gradients
+       # Get the Rayleight operators
        ROPS = REFG[5]
        
        # Compute the tendencies

computeIterativeSolveNL.py
@@ -5,14 +5,15 @@ Created on Tue Oct  1 17:05:20 2019
 
 @author: jorge.guerra
 """
-import time
 import numpy as np
 import scipy.linalg as dsl
-#import scipy.sparse as sps
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import scipy.sparse.linalg as spl
 import computeEulerEquationsLogPLogT as tendency
+
+def pause():
+    input("Press the <ENTER> key to continue...")
 
 def computeIterativeSolveNL(PHYS, REFS, REFG, DX, DZ, SOLT, INIT, udex, wdex, pdex, tdex, botdex, topdex, sysDex):
        lastSol = SOLT[:,0]
@@ -20,17 +21,18 @@ def computeIterativeSolveNL(PHYS, REFS, REFG, DX, DZ, SOLT, INIT, udex, wdex, pd
        def computeRHSUpdate(sol):
               fields, U, RdT = tendency.computePrepareFields(PHYS, REFS, sol, INIT, udex, wdex, pdex, tdex, botdex, topdex)
               rhs = tendency.computeEulerEquationsLogPLogT_NL(PHYS, REFS, REFG, fields, U, RdT, botdex, topdex)
-              rhs += tendency.computeRayleighTendency(REFG, fields, udex, wdex, pdex, tdex, botdex, topdex)
+              rhs += tendency.computeRayleighTendency(REFG, fields, botdex, topdex)
        
               return rhs
        
        def computeJacVecUpdate(sol, vec):
+              # Prepare the Jacobian
               fields, U, RdT = tendency.computePrepareFields(PHYS, REFS, sol, INIT, udex, wdex, pdex, tdex, botdex, topdex)
-              jv = tendency.computeEulerEquationsLogPLogT_NL(PHYS, REFS, REFG, fields, U, RdT, botdex, topdex)
-              jv += tendency.computeRayleighTendency(REFG, fields, udex, wdex, pdex, tdex, botdex, topdex)
+              DOPS = tendency.computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topdex)
+              # Compute the product
+              jv = tendency.computeJacobianVectorProduct(DOPS, REFG, vec, udex, wdex, pdex, tdex)
        
               return jv
-              
        
        # Solve for nonlinear equilibrium (default krylov)
        '''
@@ -47,11 +49,11 @@ def computeIterativeSolveNL(PHYS, REFS, REFG, DX, DZ, SOLT, INIT, udex, wdex, pd
        '''
        jac_options = {'jdv':computeJacVecUpdate, \
                       'method':'gmres', \
-                      'inner_maxiter':10000, \
-                      'outer_k':10}
+                      'inner_maxiter':100, \
+                      'outer_k':1}
        sol, info = opt.nonlin.nonlin_solve(computeRHSUpdate, lastSol, 
                                   jacobian=KrylovDirectJacobian(**jac_options),
-                                  iter=10, verbose=True,
+                                  iter=2, verbose=True,
                                   maxiter=100,
                                   line_search='armijo',
                                   full_output=True,
@@ -191,10 +193,24 @@ class KrylovDirectJacobian(opt.nonlin.Jacobian):
         if nv == 0:
             return 0*v
         # Compute the updated Jacobian-vector product
-        #r = self.jac_vec(self.x0, v)
+        #r1 = self.jac_vec(self.x0, v)
         sc = 0.5*self.omega / nv
         #r = (self.func(self.x0 + sc*v) - self.f0) / sc
-        r = (self.func(self.x0 + sc*v) - self.func(self.x0 - sc*v)) / sc
+        r2 = (self.func(self.x0 + sc*v) - self.func(self.x0 - sc*v)) / sc
+        '''
+        plt.figure(figsize=(12.0, 8.0))
+        varL = (int) (len(v) / 4)
+        udex = range(0, varL)
+        wdex = range(varL, 2*varL)
+        pdex = range(2*varL, 3*varL)
+        tdex = range(3*varL, len(v))
+        plt.plot(r1[tdex], 'k-'); #plt.xscale('log'); plt.yscale('linear'); #plt.ylim(1.0E-6, 1.0E-1)
+        plt.plot(r2[tdex], 'b:'); #plt.xscale('log'); plt.yscale('linear'); #plt.ylim(1.0E-6, 1.0E-1)
+        plt.show()
+        pause()
+        '''
+        r = r2
+        
         if not np.all(np.isfinite(r)) and np.all(np.isfinite(v)):
             raise ValueError('Function returned non-finite results')
         return r