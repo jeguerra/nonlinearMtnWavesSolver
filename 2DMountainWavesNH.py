Major fix to PGF terms in Jacobian.

2DMountainWavesNH.py
@@ -48,14 +48,15 @@ from computeIterativeSolveNL import computeIterativeSolveNL
 
 import faulthandler; faulthandler.enable()
 
-def displayResiduals(message, RHS, time, udex, wded, pdex, tdex):
+def displayResiduals(message, RHS, thisTime, udex, wded, pdex, tdex):
        err = np.linalg.norm(RHS)
        err1 = np.linalg.norm(RHS[udex])
        err2 = np.linalg.norm(RHS[wdex])
        err3 = np.linalg.norm(RHS[pdex])
        err4 = np.linalg.norm(RHS[tdex])
+       print(message)
        print('Time: %d, Residuals: %10.4E, %10.4E, %10.4E, %10.4E, %10.4E' \
-             % (time, err1, err2, err3, err4, err))
+             % (thisTime, err1, err2, err3, err4, err))
        
        return err
 
@@ -523,8 +524,8 @@ if __name__ == '__main__':
                      # Print out diagnostics every OTI steps
                      if tt % OTI == 0:
                             message = ''
-                            time = DT * tt
-                            err = displayResiduals(message, RHS, time, udex, wdex, pdex, tdex)
+                            thisTime = DT * tt
+                            err = displayResiduals(message, RHS, thisTime, udex, wdex, pdex, tdex)
                             error.append(err)
                      
                      if tt % ITI == 0:
@@ -559,7 +560,8 @@ if __name__ == '__main__':
               
               # Copy state instance 0 to 1
               SOLT[:,1] = np.array(SOLT[:,0])
-              
+              DSOL = SOLT[:,1] - SOLT[:,0]
+       #%%       
        endt = time.time()
        print('Solve the system: DONE!')
        print('Elapsed time: ', endt - start)

computeEulerEquationsLogPLogT.py
@@ -146,6 +146,7 @@ def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topd
        # Compute diagonal blocks related to sensible temperature
        RdT_bar = REFS[9]
        T_prime = (1.0 / Rd) * (RdT - RdT_bar[:,0])
+       T_ratio = T_prime * np.reciprocal((1.0 / Rd) * RdT_bar[:,0])
        RdT_barM = sps.diags(RdT_bar[:,0], offsets=0, format='csr')
        PtPx = DDXM.dot(T_prime) - DZDX * DDZM.dot(T_prime)
        DtDz = DDZM.dot(T_prime)
@@ -158,7 +159,7 @@ def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topd
        # Compute common horizontal transport block
        UPXM = UM.dot(DDXM) + WXZM.dot(DDZM)
        
-       unit = sps.identity(len(U))
+       bf = sps.diags(T_ratio + 1.0, offsets=0, format='csr')
        
        # Compute the blocks of the Jacobian operator
        LD11 = UPXM + PuPxM
@@ -169,7 +170,7 @@ def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, RdT, botdex, topd
        LD21 = PwPxM # vanish initial
        LD22 = UPXM + DwDzM
        LD23 = RdTM.dot(DDZM) + RdT_barM.dot(DLTDZM) + Rd * DtDzM
-       LD24 = RdTM.dot(DlpDzM) - gc * unit
+       LD24 = RdTM.dot(DlpDzM) - gc * bf
        
        LD31 = gam * PPXM + PlpPxM
        LD32 = gam * DDZM + DlpDzM + DLPDZM