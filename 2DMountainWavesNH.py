MAJOR FIXES: Stable solution with QS derivatives in the horizontal.

2DMountainWavesNH.py
@@ -550,8 +550,8 @@ def runModel(TestName):
        ROPS, RLM, GML, LDEX = computeRayleighEquations(DIMS, REFS, ZRL, RLOPT, ubdex, utdex)
        
        # Make a collection for background field derivatives
-       REFG = [GML, DLTDZ, DQDZ, RLOPT[4], RLM, LDEX]
-       
+       REFG = [GML, DLTDZ, DQDZ, RLOPT[4], RLM, LDEX.flatten()]
+              
        # Update the REFS collection
        REFS.append(np.reshape(UZ, (OPS,), order='F')) # index 8
        REFS.append((np.reshape(PORZ, (OPS,), order='F'), np.reshape(PBAR, (OPS,), order='F'))) #index 9
@@ -588,7 +588,7 @@ def runModel(TestName):
        DDXMD, DDZMD = devop.computePartialDerivativesXZ(DIMS, REFS[7], DDX_QS, DDZ_QS)
        
        # X partial derivatives
-       PPXMS = DDXMS - sps.diags(np.reshape(DZT, (OPS,), order='F')).dot(DDZMS)
+       PPXMS = DDXMS2 - sps.diags(np.reshape(DZT, (OPS,), order='F')).dot(DDZMS2)
        PPXMD = DDXMD - sps.diags(np.reshape(DZT, (OPS,), order='F')).dot(DDZMD)
        
        #'''
@@ -680,7 +680,7 @@ def runModel(TestName):
        # Store the terrain profile
        REFS.append(DZT) # index 14
        REFS.append(np.reshape(DZT, (OPS,1), order='F')) # index 15
-       REFS.append(sps.csr_matrix(DDX_1D)) # index 16
+       REFS.append(sps.csr_matrix(DDX_QS)) # index 16
        REFS.append(sps.csr_matrix(DDX_QS)) # index 17
        
        if not StaticSolve:
@@ -736,8 +736,8 @@ def runModel(TestName):
               print('Z: ', DZ_spr)
               '''
               # Diffusion filter grid length based on resolution powers
-              DL2 = 1.0 * abs(DZ_avg)
-              DL1 = 1.0 * abs(DX_avg)
+              DL2 = 1.0 * DZ_max
+              DL1 = 1.0 * DX_avg
               DL_MS = 0.5 * (DL1**2 + DL2**2)
               DL_RMS = mt.sqrt(DL_MS)
               DL_GM = mt.sqrt(DL1 * DL2)
@@ -846,7 +846,7 @@ def runModel(TestName):
               
               #'''
               # Compute the RHS for this iteration
-              rhs, DqDx, DqDz, U = eqs.computeRHS(fields, hydroState, REFS[10][0], REFS[10][1], REFS[6][0], \
+              rhs, DqDx, DqDz = eqs.computeRHS(fields, hydroState, REFS[10][0], REFS[10][1], REFS[6][0], \
                                                   PHYS, REFS, REFG, ebcDex, zeroDex, True)
               RHS = np.reshape(rhs, (physDOF,), order='F')
               err = displayResiduals('Current function evaluation residual: ', RHS, 0.0, udex, wdex, pdex, tdex)
@@ -995,7 +995,7 @@ def runModel(TestName):
               #%% Set the output residual and check
               message = 'Residual 2-norm BEFORE Newton step:'
               err = displayResiduals(message, RHS, 0.0, udex, wdex, pdex, tdex)
-              rhs, DqDx, DqDz, U = eqs.computeRHS(fields, hydroState, REFS[10][0], REFS[10][1], REFS[6][0], \
+              rhs, DqDx, DqDz = eqs.computeRHS(fields, hydroState, REFS[10][0], REFS[10][1], REFS[6][0], \
                                                   PHYS, REFS, REFG, ebcDex, zeroDex, True)
               RHS = np.reshape(rhs, (physDOF,), order='F')
               message = 'Residual 2-norm AFTER Newton step:'
@@ -1057,7 +1057,7 @@ def runModel(TestName):
                      if ti % OTI == 0:
                      
                             # Compute the updated RHS
-                            rhsVec, DqDx, DqDz, U = eqs.computeRHS(fields, hydroState, REFS[13][0], REFS[13][1], REFS[6][0], \
+                            rhsVec, DqDx, DqDz = eqs.computeRHS(fields, hydroState, REFS[13][0], REFS[13][1], REFS[6][0], \
                                                                 PHYS, REFS, REFG, ebcDex, zeroDex, True)
                             
                             message = ''

computeAdjust4CBC.py
@@ -38,7 +38,7 @@ def computeAdjust4CBC(DIMS, numVar, varDex, bcType):
        # Index all boundary DOF that can be diffused on
        diffDex = (uldex1, urdex1, ubdex, utdex)
        
-       isInflow = True
+       isInflow = False
        if isInflow:
               print('Inflow boundary configuration.')
        else:
@@ -80,7 +80,7 @@ def computeAdjust4CBC(DIMS, numVar, varDex, bcType):
                      top = utdex + iW*OPS
                      rowsOutBC_static = set(np.concatenate((left, top)))
               else:
-                     # Inflow condition on UWPT TRANSIENT SOLUTION
+                     # Pinned condition on UWPT TRANSIENT SOLUTION
                      rowsOutU = set(np.concatenate((uldex1,urdex1)))
                      rowsOutW = set(np.concatenate((uldex1,urdex1,utdex)))
                      rowsOutP = set(np.concatenate((uldex1,urdex1)))

computeEulerEquationsLogPLogT.py
@@ -103,7 +103,11 @@ def computeFieldDerivatives2(PqPx, PqPz, DDX, DDZ, REFS, REFG, DCF, isFluxDiv):
        PqPz += DQDZ
        
        if isFluxDiv:
-              vd = np.hstack((DCF[0] * PqPx, DCF[1] * PqPz))
+              try:
+                     vd = np.hstack((np.expand_dims(DCF[0], axis=1) * PqPx, \
+                                     np.expand_dims(DCF[1], axis=1) * PqPz))
+              except FloatingPointError:
+                     vd = np.hstack((0.0 * PqPx, 0.0 * PqPz))
        else:
               vd = np.hstack((PqPx, PqPz))
        dvdx, dvdz = computeFieldDerivatives(vd, DDX, DDZ)
@@ -136,13 +140,13 @@ def computeRHS(fields, hydroState, DDX, DDZ, dhdx, PHYS, REFS, REFG, ebcDex, zer
        DqDx, DqDz = \
               computeFieldDerivatives(fields, DDX, DDZ)
        rhsVec = computeEulerEquationsLogPLogT_Explicit(PHYS, DqDx, DqDz, REFS, REFG, \
-                                                     fields, Q[:,0], Q[:,1])
+                                                     fields, Q[:,0], Q[:,1], ebcDex)
        if withRay:
               rhsVec += computeRayleighTendency(REFG, fields)
        
        rhsVec = enforceTendencyBC(rhsVec, zeroDex, ebcDex, dhdx)
        
-       return rhsVec, DqDx, DqDz, Q[:,0]
+       return rhsVec, DqDx, DqDz
 
 #%% Evaluate the Jacobian matrix
 def computeJacobianMatrixLogPLogT(PHYS, REFS, REFG, fields, U, botdex, topdex):
@@ -323,7 +327,7 @@ def computeEulerEquationsLogPLogT_Classical(DIMS, PHYS, REFS, REFG):
        return DOPS
 
 # Fully explicit evaluation of the non linear equations (dynamic components)
-def computeEulerEquationsLogPLogT_Explicit(PHYS, PqPx, DqDz, REFS, REFG, fields, U, W):
+def computeEulerEquationsLogPLogT_Explicit(PHYS, PqPx, DqDz, REFS, REFG, fields, U, W, ebcDex):
        # Get physical constants
        gc = PHYS[0]
        kap = PHYS[4]
@@ -356,9 +360,20 @@ def computeEulerEquationsLogPLogT_Explicit(PHYS, PqPx, DqDz, REFS, REFG, fields,
        # Compute local divergence
        divergence = PqPx[:,0] + DqDz[:,1]
        
+       divergence[ebcDex[0]] = PqPx[ebcDex[0],0]
+       divergence[ebcDex[1]] = PqPx[ebcDex[1],0]
+       
        # Compute pressure gradient forces
        pgradx = RdT * PqPx[:,2]
        pgradz = RdT * DqDz[:,2] - gc * T_ratio
+       
+       pgradx[ebcDex[0]] = 0.0
+       pgradx[ebcDex[1]] = 0.0
+       
+       pgradz[ebcDex[0]] = 0.0
+       pgradz[ebcDex[1]] = 0.0
+       pgradz[ebcDex[2]] = 0.0
+       pgradz[ebcDex[3]] = 0.0
        
        DqDt = -(Uadvect + Wadvect)
        
@@ -385,13 +400,12 @@ def computeRayleighTendency(REFG, fields):
               DqDt = -mu * ROP.dot(fields)
        except FloatingPointError:
               DqDt = np.zeros(fields.shape)
-              
+       
+       #DqDt[:,2] = 0.0       
+       
        return DqDt
 
 def computeDiffusionTendency(q, PqPx, PqPz, P2qPx2, P2qPz2, P2qPzx, P2qPxz, REFS, REFG, ebcDex, DLD, DCF, isFluxDiv):
-       
-       dx = DLD[0]
-       dz = DLD[1]
        
        dhdx = REFS[6][0]
        metrics = REFS[6][1]
@@ -402,27 +416,27 @@ def computeDiffusionTendency(q, PqPx, PqPz, P2qPx2, P2qPz2, P2qPzx, P2qPxz, REFS
        tdex = ebcDex[3]
        
        DqDt = np.zeros(P2qPx2.shape)
+       
+       DC1 = DCF[0] # coefficient to the X direction flux
+       DC2 = DCF[1] # coefficient to the Z direction flux
+              
+       try:
+           #mu_xb = DC1[bdex]
+           #mu_xt = DC1[tdex]
+           
+           #mu_xb = np.linalg.norm(np.stack((DC1[bdex],DC2[bdex]), axis=1), axis=1)
+           #mu_xt = np.linalg.norm(np.stack((DC1[tdex],DC2[tdex]), axis=1), axis=1)
+           
+           mu_xb = np.reciprocal(S) * DC1[bdex]
+           mu_xt = DC1[tdex]
+       except FloatingPointError:
+           mu_xb = np.zeros(bdex.shape)
+           mu_xt = np.zeros(tdex.shape)
+           
        if isFluxDiv:
               DC1 = 1.0
               DC2 = 1.0
-              mu_xt = 1.0
-              mu_xb = 1.0
-       else:
-              DC1 = DCF[0][:,0] # coefficient to the X direction flux
-              DC2 = DCF[1][:,0] # coefficient to the Z direction flux
               
-              try:
-                  #mu_xb = DC1[bdex]
-                  #mu_xt = DC1[tdex]
-                  
-                  #mu_xb = np.linalg.norm(np.stack((DC1[bdex],DC2[bdex]), axis=1), axis=1)
-                  #mu_xt = np.linalg.norm(np.stack((DC1[tdex],DC2[tdex]), axis=1), axis=1)
-                  
-                  mu_xb = np.reciprocal(S) * DC1[bdex]
-                  mu_xt = DC1[tdex]
-              except FloatingPointError:
-                  mu_xb = np.zeros(bdex.shape)
-                  mu_xt = np.zeros(tdex.shape)
        try:
               #%% INTERIOR DIFFUSION
               # Diffusion of u-w vector

computeRayleighEquations.py
@@ -41,7 +41,6 @@ def computeRayleighField(DIMS, REFS, height, width, applyTop, applyLateral):
        RL = np.zeros((NZ, NX))
        RLX = np.zeros((NZ, NX))
        RLZ = np.zeros((NZ, NX))
-       SBR = np.ones((NZ, NX))
        
        for ii in range(0,NZ):
               for jj in range(0,NX):
@@ -80,9 +79,6 @@ def computeRayleighField(DIMS, REFS, height, width, applyTop, applyLateral):
                      RLX[ii,jj] = RFX
                      RLZ[ii,jj] = RFZ
                      RL[ii,jj] = np.amax([RFX, RFZ])
-                     
-                     # Set the binary matrix
-                     SBR[ii,jj] = isRL
        
        '''
        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -158,7 +154,7 @@ def computeRayleighField(DIMS, REFS, height, width, applyTop, applyLateral):
        plt.show()
        input()
        '''                  
-       return (GML, GMLX, GMLZ), RL, RLX, RLZ, SBR
+       return (GML, GMLX, GMLZ), RL, RLX, RLZ
 
 def computeRayleighEquations(DIMS, REFS, depth, RLOPT, topdex, botdex):
        # Get options data
@@ -171,7 +167,7 @@ def computeRayleighEquations(DIMS, REFS, depth, RLOPT, topdex, botdex):
        OPS = DIMS[5]
        
        # Set up the Rayleigh field
-       GML, RL, RLX, RLZ, SBR = computeRayleighField(DIMS, REFS, depth, width, \
+       GML, RL, RLX, RLZ = computeRayleighField(DIMS, REFS, depth, width, \
                                                      applyTop, applyLateral)
        
        # Compute the diagonal for full Rayleigh field
@@ -192,8 +188,7 @@ def computeRayleighEquations(DIMS, REFS, depth, RLOPT, topdex, botdex):
        ROPS = mu * np.array([RLM, RLM, RLM, RLM])
        
        # Get the indices for the layer regions
-       SBRV = np.reshape(SBR, (OPS,), order='F')
-       ldex = np.argwhere(SBRV)
+       ldex = np.argwhere(tempDiagonal)
        
        return ROPS, RLM, GML, ldex
        

computeResidualViscCoeffs.py
@@ -49,20 +49,19 @@ def computeResidualViscCoeffs(DIMS, RES, QM, VFLW, DLD, DLD2, NE):
 
        return (np.expand_dims(CRES, axis=1), np.expand_dims(CRES, axis=1))
 
-def computeResidualViscCoeffsRaw(DIMS, RES, dstate, state, hydroState, DLD, dhdx, bdex):
+def computeResidualViscCoeffsRaw(DIMS, RES, qnorm, state, DLD, dhdx, bdex, ldex):
        
        # Compute flow speed
-       UD = np.abs(state[:,0] + hydroState[:,0])
+       UD = np.abs(state[:,0])
        WD = np.abs(state[:,1])
        
        # Compute field normalization
-       QM = bn.nanmax(np.abs(dstate), axis=0)
+       QM = bn.nanmax(np.abs(qnorm), axis=0)
        
        try:
               # Compute the flow magnitude
               vel = np.stack((UD, WD),axis=1)
               VFLW = np.linalg.norm(vel, axis=1)
-              #VFLWM = bn.nanmax(VFLW) * np.ones(WD.shape)
               
               # Compute absolute value of residuals
               ARES = np.abs(RES)
@@ -79,26 +78,40 @@ def computeResidualViscCoeffsRaw(DIMS, RES, dstate, state, hydroState, DLD, dhdx
               QRES_MAX = bn.nanmax(ARES, axis=1)
               
               # Compute upper bound on coefficients based on flow speed
-              QMAX1 = 0.5 * DLD[0] * VFLW
-              QMAX2 = 0.5 * DLD[1] * VFLW
-              QMAXB = QMAX1[bdex]
-              #QMAX1 = 0.5 * UD
-              #QMAX2 = 0.5 * WD
-              #QMAX1[QMAX1 <= 1.0E-10] = np.inf
-              #QMAX2[QMAX2 <= 1.0E-10] = np.inf
+              QMAX1 = 0.5 * DLD[0] * UD
+              QMAX2 = 0.5 * DLD[1] * WD
+              
+              QB1 = bn.nanmax(QMAX1)
+              QB2 = bn.nanmax(QMAX2)
               
               # Limit DynSGS to upper bound
+              CRES1 = DLD[0]**2 * QRES_MAX
+              CRES1[CRES1 > QB1] = QB1
+              CRES2 = DLD[1]**2 * QRES_MAX
+              CRES2[CRES2 > QB2] = QB2
+              
+              # Set maximum adaptive damping along the terrain
+              CRES1[bdex] = QMAX1[bdex]
+              CRES2[bdex] = QMAX2[bdex]
+              '''
               compare = np.stack((DLD[0]**2 * QRES_MAX, QMAX1),axis=1)
               CRES1 = np.expand_dims(bn.nanmin(compare, axis=1), axis=1)
-              CRES1[bdex,0] = QMAXB
               
               compare = np.stack((DLD[1]**2 * QRES_MAX, QMAX2),axis=1)
               CRES2 = np.expand_dims(bn.nanmin(compare, axis=1), axis=1)
-              CRES2[bdex,0] = QMAXB
+              '''
+              '''
+              # Set maximum adaptive damping along the terrain
+              CRES1[bdex] = 0.5 * DLD[0] * VFLW[bdex]
+              CRES2[bdex] = 0.5 * DLD[1] * VFLW[bdex]
               
+              # Set maximum adaptive damping in the Rayleigh layers
+              CRES1[ldex] = 0.5 * DLD[0] * VFLW[ldex]
+              CRES2[ldex] = 0.5 * DLD[1] * VFLW[ldex]
+              '''
        except FloatingPointError:
-              CRES1 = np.zeros((state.shape[0],1))
-              CRES2 = np.zeros((state.shape[0],1))
+              CRES1 = np.zeros((state.shape[0]))
+              CRES2 = np.zeros((state.shape[0]))
        
        return (CRES1, CRES2)
 

computeTimeIntegration.py
@@ -46,8 +46,13 @@ def computeTimeIntegrationNL(DIMS, PHYS, REFS, REFG, DLD, TOPT, \
        order = TOPT[3]
        mu = REFG[3]
        RLM = REFG[4]
+       ldex = REFG[5]
        dhdx = REFS[6][0]       
        diffusiveFlux = False
+       
+       # Normalization and bounding to DynSGS
+       state = sol0 + init0
+       qnorm = (sol0 - bn.nanmean(sol0))
        
        #'''
        if isFirstStep:
@@ -72,9 +77,8 @@ def computeTimeIntegrationNL(DIMS, PHYS, REFS, REFG, DLD, TOPT, \
               DF = coeff * DT
               RayDamp = np.reciprocal(1.0 + DF * mu * RLM.data)
               
-              # Compute flow field
-              U = solA[:,0] + init0[:,0]
-              W = solA[:,1]
+              # Compute full state
+              Q = solA + init0
               
               # Compute first derivatives and RHS with spectral operators
               if verticalStagger:
@@ -82,7 +86,7 @@ def computeTimeIntegrationNL(DIMS, PHYS, REFS, REFG, DLD, TOPT, \
               else:
                      DqDxA, DqDzA = tendency.computeFieldDerivatives(solA, DDXM_A, DDZM_A)
               
-              args1 = [PHYS, DqDxA, DqDzA, REFS, REFG, solA, U, W]
+              args1 = [PHYS, DqDxA, DqDzA, REFS, REFG, solA, Q[:,0], Q[:,1], ebcDex]
               rhsExp = tendency.computeEulerEquationsLogPLogT_Explicit(*args1)
               rhsExp = tendency.enforceTendencyBC(rhsExp, zeroDex, ebcDex, dhdx)
               
@@ -91,27 +95,25 @@ def computeTimeIntegrationNL(DIMS, PHYS, REFS, REFG, DLD, TOPT, \
               except FloatingPointError:
                   solB = sol2Update + 0.0
               
+              U = solB[:,0] + init0[:,0]
               solB = tendency.enforceEssentialBC(solB, U, zeroDex, ebcDex, dhdx)
               
               # Apply Rayleigh damping layer implicitly
-              solB = np.copy(RayDamp.T * solB)
+              rdex = [0, 1, 2, 3]
+              solB[:,rdex] = np.copy(RayDamp.T * solB[:,rdex])
               
               # Update the adaptive coefficients using residual
-              rhsNew, DqDxR, DqDzR, U = tendency.computeRHS(solB, init0, DDXM_B, DDZM_B, dhdx, PHYS, REFS, REFG, ebcDex, zeroDex, True)
-              
-              #dsol = np.copy(solB)
-              dsol = (solB - np.nanmean(solB))
-              #dsol = (solB - solA)
+              rhsNew, DqDxR, DqDzR = tendency.computeRHS(solB, init0, DDXM_B, DDZM_B, dhdx, PHYS, REFS, REFG, ebcDex, zeroDex, False)
               
               if DynSGS_RES:
                      resField = rhsNew - rhsExp
               else:
                      resField = np.copy(rhsNew)
-       
+              
               if filteredCoeffs:
-                     DCF = rescf.computeResidualViscCoeffsFiltered(DIMS, resField, dsol, solB, init0, DLD, NE)
+                     DCF = rescf.computeResidualViscCoeffsFiltered(DIMS, resField, qnorm, state, DLD, NE)
               else:
-                     DCF = rescf.computeResidualViscCoeffsRaw(DIMS, resField, dsol, solB, init0, DLD, REFS[6][0], ebcDex[2])
+                     DCF = rescf.computeResidualViscCoeffsRaw(DIMS, resField, qnorm, state, DLD, dhdx, ebcDex[2], ldex)
               del(resField)
               
               # Compute diffusive tendency
@@ -123,9 +125,13 @@ def computeTimeIntegrationNL(DIMS, PHYS, REFS, REFG, DLD, TOPT, \
               rhsDif = tendency.enforceTendencyBC(rhsDif, zeroDex, ebcDex, REFS[6][0])
               
               # Apply diffusion update
-              solB += DF * rhsDif
+              try:
+                     solB += DF * rhsDif
+              except FloatingPointError:
+                     solB += 0.0
               
               # Clean up on essential BC
+              U = solB[:,0] + init0[:,0]
               solB = tendency.enforceEssentialBC(solB, U, zeroDex, ebcDex, dhdx)
                                           
               return solB