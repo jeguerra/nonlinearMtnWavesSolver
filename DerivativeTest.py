Higher resolution, fixes to cubic/quintic derivatives BC

DerivativeTest.py
@@ -204,11 +204,11 @@ print('Compact FD6: ', np.count_nonzero(DDZ_CFD2))
 print('Compact FD8: ', np.count_nonzero(DDZ_CFD3))
 
 # Take boundary information from compact FD operators
-DDZ_CS, DDZ2_CS = derv.computeCubicSplineDerivativeMatrix(zv, True, False, DDZ_CFD2)
-DDZ_QS, DDZ4_QS = derv.computeQuinticSplineDerivativeMatrix(zv, True, False, DDZ_CFD3)
+DDZ_CS, DDZ2_CS = derv.computeCubicSplineDerivativeMatrix(zv, False, True, DDZ_CFD2)
+DDZ_QS, DDZ4_QS = derv.computeQuinticSplineDerivativeMatrix(zv, False, True, DDZ_CFD3)
 
 # SUPER-RESOLUTION SPECTRAL DERIVATIVE
-DDZ_AV = 0.5 * (DDZ_CS + DDZ_QS) 
+DDZ_AV = 0.5 * (DDZ_CFD2 + DDZ_QS) 
 
 # Compute eigenspectra
 W1 = scl.eigvals(DDZ_CFD1)

computeDerivativeMatrix.py
@@ -728,38 +728,44 @@ def computeQuinticSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
        D1A = DDM_BC[0,:] # left end 1st derivative
        D1B = DDM_BC[-1,:] # right end 1st derivative
        
+       h0 = abs(dom[1] - dom[0])
+       hn = abs(dom[-1] - dom[-2])
+       
        if isClamped:
               A[0,0] = 1.0
               B[0,:] = D4A
               A[-1,-1] = 1.0
               B[-1,:] = D4B
-       elif isEssential:
-              A[0,0] = 1.0 * dom[1] / (dom[1] - dom[0]) 
-              A[0,1] = -1.0 * dom[0] / (dom[1] - dom[0])
-              B[0,:] = np.zeros(N)
-              A[N-1,N-2] = -1.0 * dom[-1] / (dom[-2] - dom[-1])
-              A[N-1,N-1] = 1.0 * dom[-2] / (dom[-2] - dom[-1])
-              B[N-1,:] = np.zeros(N)
-       else:
-              A[0,0] = 1.0
-              B[0,:] = D4A
-              A[-1,-1] = 1.0
-              B[-1,:] = D4B
               
-       # Compute the 4th derivative matrix
-       Q, R = scl.qr(A)
-       PLU = scl.lu_factor(R)
-       AIB = scl.lu_solve(PLU, (Q.T).dot(B))
+              Q, R = scl.qr(A)
+              PLU = scl.lu_factor(R)
+              AIB = scl.lu_solve(PLU, (Q.T).dot(B))
+       elif isEssential:
+              A[0,0] = +1.0 * dom[1] / h0
+              A[0,1] = -1.0 * dom[0] / h0
+              B[0,:] = np.zeros(N)
+              
+              A[N-1,N-2] = +1.0 * dom[-1] / hn
+              A[N-1,N-1] = -1.0 * dom[-2] / hn
+              B[N-1,:] = np.zeros(N)
+              
+              Q, R = scl.qr(A)
+              PLU = scl.lu_factor(R)
+              AIB = scl.lu_solve(PLU, (Q.T).dot(B))
+       else:
+              # NATURAL cubic spline.
+              AIB = np.diag(np.ones(N))
+              AIB[1:N-1,1:N-1] = np.linalg.solve(A[1:-1,1:-1], B[1:-1,1:-1])
        
        # Compute the 1st derivative matrix
        DDM = C.dot(AIB) + D
-       #'''
+
        # Set boundary derivatives from specified
        if isClamped or not isEssential:
               DDM[0,:] = D1A
               DDM[-1,:] = D1B
-       #'''
-       #DDM1 = removeLeastSingularValue(DDM)
+       #else:
+              
        DDM = numericalCleanUp(DDM)
                      
        return DDM, AIB
@@ -779,6 +785,10 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
               else:
                      print('Setting derivatives to single values (not 0) at ends NOT SUPPORTED!')
                      DDM_BC = np.zeros((2,N))
+              
+              DM2 = DDM_BC * DDM_BC
+       else:
+              DM2 = DDM_BC.dot(DDM_BC)
                      
        # Loop over each interior point in the irregular grid
        for ii in range(1,N-1):
@@ -794,7 +804,7 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
               B[ii,ii] = (1.0 / hm + 1.0 / hp)
               B[ii,ii+1] = -1.0 / hp
               
-       for ii in range(1,N-1):
+       for ii in range(0,N-1):
               hp = abs(dom[ii+1] - dom[ii])
               C[ii,ii] = -1.0 / 3.0 * hp
               C[ii,ii+1] = -1.0 / 6.0 * hp
@@ -802,80 +812,51 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
               D[ii,ii] = -1.0 / hp
               D[ii,ii+1] = 1.0 / hp
               
-       # Compute BC adjustments
        h0 = abs(dom[1] - dom[0])
        hn = abs(dom[-1] - dom[-2])
        
+       # Prescribed derivative conditions
+       D2A = DM2[0,:] # left end 4th derivative
+       D2B = DM2[-1,:] # right end 4th derivative
+       D1A = DDM_BC[0,:] # left end 1st derivative
+       D1B = DDM_BC[-1,:] # right end 1st derivative
+       
        if isClamped:
-              # Left end
-              A[0,0] = -2.0 * h0 / 3.0
-              A[0,1] = h0 / 6.0
+              A[0,0] = 1.0
+              B[0,:] = D2A
+              A[-1,-1] = 1.0
+              B[-1,:] = D2B
               
-              # Use derivative by CFD to set boundary condition
-              B[0,:] = DDM_BC[0,:]
-              B[0,0] += 1.0 / h0
-              B[0,1] -= 1.0 / h0
-              
-              # Right end
-              A[N-1,N-2] = -hn / 6.0
-              A[N-1,N-1] = 2.0 * hn / 3.0
-              
-              # Use derivative by CFD to set boundary condition
-              B[N-1,:] = DDM_BC[-1,:]
-              B[N-1,N-2] += 1.0 / hn
-              B[N-1,N-1] -= 1.0 / hn
-              #'''
-              
-              # Compute the first derivative matrix
-              AIB = np.linalg.solve(A, B)
-              DDM = C.dot(AIB) + D
-              
-              # Adjust ends
-              DDM[0,:] = DDM_BC[0,:]
-              DDM[-1,:] = DDM_BC[-1,:]
-              
+              Q, R = scl.qr(A)
+              PLU = scl.lu_factor(R)
+              AIB = scl.lu_solve(PLU, (Q.T).dot(B))
        elif isEssential:
-              # Left end
-              A[0,0] = -1.0 / h0
-              A[0,1] = 1.0 / h0
+              A[0,0] = +1.0 / h0
+              A[0,1] = -1.0 / h0
               B[0,:] = np.zeros(N)
               
-              # Right end
-              A[N-1,N-2] = 1.0 / hn
+              A[N-1,N-2] = +1.0 / hn
               A[N-1,N-1] = -1.0 / hn
               B[N-1,:] = np.zeros(N)
               
-              # Compute the first derivative matrix
-              AIB = np.linalg.solve(A, B)
-              DDM = C.dot(AIB) + D
-              
-              # Adjust the ends
-              DDM[0,:] = h0 / 6.0 * AIB[1,:] - 2.0 * h0 / 3.0 * AIB[0,:]
-              DDM[0,0] -= 1.0 / h0
-              DDM[0,1] += 1.0 / h0
-              
-              DDM[N-1,:] = -h0 / 6.0 * AIB[-2,:] + 2.0 * hn / 3.0 * AIB[-1,:]
-              DDM[N-1,N-2] -= 1.0 / hn
-              DDM[N-1,N-1] += 1.0 / hn
-       
+              Q, R = scl.qr(A)
+              PLU = scl.lu_factor(R)
+              AIB = scl.lu_solve(PLU, (Q.T).dot(B))
        else:
               # NATURAL cubic spline.
-              AIB = np.zeros((N,N))
+              AIB = np.diag(np.ones(N))
               AIB[1:N-1,1:N-1] = np.linalg.solve(A[1:-1,1:-1], B[1:-1,1:-1])
-              DDM = C.dot(AIB) + D
-              
-              # Adjust the ends
-              DDM[0,:] = h0 / 6.0 * AIB[1,:]
-              DDM[0,0] -= 1.0 / h0
-              DDM[0,1] += 1.0 / h0
-              
-              DDM[N-1,:] = -h0 / 6.0 * AIB[-2,:]
-              DDM[N-1,N-2] -= 1.0 / hn
-              DDM[N-1,N-1] += 1.0 / hn      
        
-       #DDM1 = removeLeastSingularValue(DDM)
-       DDM = numericalCleanUp(DDM)
+       # Compute the 1st derivative matrix
+       DDM = C.dot(AIB) + D
 
+       # Set boundary derivatives from specified
+       if isClamped or not isEssential:
+              DDM[0,:] = D1A
+              DDM[-1,:] = D1B
+       #else:
+              
+       DDM = numericalCleanUp(DDM)
        return DDM, AIB
 
 # Computes standard 4th order compact finite difference 1st derivative matrix

computeEulerEquationsLogPLogT.py
@@ -60,15 +60,15 @@ def computeRdT(lp, lt, RdT_bar, kap):
        
        # Compute pressure gradient force scaling (buoyancy)              
        earg = kap * lp + lt
-       T_ratio = np.expm1(earg, dtype=np.float64)
+       T_ratio = np.expm1(earg, dtype=np.longdouble)
        #T_exp = np.exp(earg, dtype=np.longdouble)                 
               
        RdT = RdT_bar * (T_ratio + 1.0)
        #RdT = RdT_bar * T_exp
        #T_ratio = T_exp - 1.0
                      
-       #return RdT.astype(np.float64), T_ratio.astype(np.float64)
-       return RdT, T_ratio
+       return RdT.astype(np.float64), T_ratio.astype(np.float64)
+       #return RdT, T_ratio
 
 def computeFieldDerivatives(q, DDX, DDZ, RSBops):
                      

transientPlot.py
@@ -18,11 +18,11 @@ from netCDF4 import Dataset
 from joblib import Parallel, delayed
 
 m2k = 1.0E-3
-runPertb = True
+runPertb = False
 runSGS = False
 runPar = False
 imgname = '/media/jeguerra/FastDATA/linearMtnWavesSolver/animations/toanimate'
-fname = 'Simulation2View2.nc'
+fname = 'Simulation2View_150m.nc'
 m_fid = Dataset(fname, 'r', format="NETCDF4")
 
 times = m_fid.variables['time'][:]
@@ -44,14 +44,13 @@ dlnt = m_fid.variables['Dln_tDt'][:,:,:,0]
 TH = np.exp(lnt)
 th = TH - np.exp(LNT)
 
-plotPert = True
-if plotPert:
+if runPertb:
        var2plot = th
        cmp2plot = 'RdBu_r'
        out_name = 'PerturbationPT01.gif'
 else:
        var2plot = TH
-       cmp2plot = 'nipy_spectral_r'
+       cmp2plot = 'gist_ncar_r'
        out_name = 'TotalPT01.gif'
 
 # Get the upper and lower bounds for TH
@@ -62,26 +61,14 @@ print('Plot bounds: ', clim1, clim2)
 def plotPertb(tt):
        
        th2plot = np.ma.getdata(var2plot[tt,:,:])
-       '''
-       mr = 20
-       nm = th2plot.shape
-       U, s, Vh = scl.svd(th2plot)
-       S = scl.diagsvd(s[0:mr], mr, mr)
-       th2plot = U[:,0:mr].dot(S)
-       th2plot = th2plot.dot(Vh[0:mr,:])
-       '''
-       thisFigure = plt.figure(figsize=(16.0, 8.0))
+       
+       thisFigure = plt.figure(figsize=(24.0, 8.0))
        plt.grid(visible=None, which='major', axis='both', color='k', linestyle='--', linewidth=0.25)
        plt.contourf(1.0E-3*X, 1.0E-3*Z, th2plot, 256, cmap=cmp2plot, vmin=clim1, vmax=clim2)
        
-       #norm = mpl.colors.Normalize(vmin=-clim, vmax=clim)
-       #plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cm.seismic), format='%.2e', cax=plt.gca())
-       
-       #plt.contour(m2k * X, m2k * Z, TH[tt,:,:], 21, colors='black', linewidths=1.0)
-       
        plt.fill_between(m2k * X[0,:], m2k * Z[0,:], color='black')
        plt.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True)
-       plt.xlim(-40.0, 80.0)
+       plt.xlim(-30.0, 60.0)
        plt.ylim(0.0, 16.0)
        plt.title('Total ' + r'$\theta$ and $\Delta \theta$ (K)' + \
                  ' Hour: {timeH:.2f}'.format(timeH = times[tt] / 3600.0))
@@ -103,18 +90,6 @@ def plotPertb(tt):
        print('Hour: {timeH:.2f}'.format(timeH = times[tt] / 3600.0))
        
        return image
-
-#%% Contour animation of perturbation potential temperatures
-if runPertb:    
-              
-       # Parallel processing
-       if runPar:
-              print('Attempt parallel processing...')
-              imglist = Parallel(n_jobs=8)(delayed(plotPertb)(tt) for tt in range(len(times)))
-       else:
-              print('Run serial processing...')
-              imglist = [plotPertb(tt) for tt in range(len(times))]
-              #imglist = [plotPertb(tt) for tt in range(420)]
        
 #%% Contour animation of the normalized SGS
 if runSGS:
@@ -174,5 +149,15 @@ if runSGS:
               os.remove(imgname)
               plt.close('all')
               del(fig)
+#%% Contour animation of perturbation potential temperatures
+else:    
+       # Parallel processing
+       if runPar:
+              print('Attempt parallel processing...')
+              imglist = Parallel(n_jobs=8)(delayed(plotPertb)(tt) for tt in range(len(times)))
+       else:
+              print('Run serial processing...')
+              #imglist = [plotPertb(tt) for tt in range(len(times))]
+              imglist = [plotPertb(tt) for tt in range(900)]
        
 imglist[0].save(out_name,append_images=imglist[1:], save_all=True, optimize=False, duration=30, loop=0)