Major fixes to CS/QS derivatives
stable 10 hour run

2DMountainWavesNH.py
@@ -656,10 +656,14 @@ def runModel(TestName):
        # Compute thermodynamic gradients (no interpolation!)
        PORZ = PHYS[3] * TZ
        PBAR = np.exp(LOGP) # Hydrostatic pressure
-       DLPDZ = -PHYS[0] / PHYS[3] * np.reciprocal(TZ)
+       DLPDZ = -PHYS[0] / (PHYS[3] * TZ)
        DLTDZ = np.reciprocal(TZ) * DTDZ
        DLPTDZ = DLTDZ - PHYS[4] * DLPDZ
        
+       #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+       #ax.plot_surface(REFS[4], REFS[5], DLPDZ, cmap=cm.jet,
+       #                linewidth=0, antialiased=False)
+
        # Get the static vertical gradients and store
        DUDZ = np.reshape(DUDZ, (OPS,1), order='F')
        DLTDZ = np.reshape(DLTDZ, (OPS,1), order='F')
@@ -675,6 +679,45 @@ def runModel(TestName):
        plt.show()
        print('Check interpolated background fields...')
        '''
+       
+       physDOF = numVar * OPS
+       lmsDOF = len(ubdex)
+       
+       # Initialize solution storage
+       SOLT = np.zeros((physDOF, 2))
+       
+       # Initialize Lagrange Multiplier storage
+       LMS = np.zeros(lmsDOF)
+       
+       # Initialize hydrostatic background
+       INIT = np.zeros((physDOF,))
+       RHS = np.zeros((physDOF,))
+       
+       # Initialize the Background fields
+       INIT[udex] = np.reshape(UZ, (OPS,), order='F')
+       INIT[wdex] = np.zeros((OPS,))
+       INIT[pdex] = np.reshape(LOGP, (OPS,), order='F')
+       INIT[tdex] = np.reshape(LOGT, (OPS,), order='F')
+       hydroState = np.reshape(INIT, (OPS, numVar), order='F')
+       
+       if isRestart and StaticSolve:
+              print('Restarting from previous solution...')
+              SOLT, LMS, DCF, NX_in, NZ_in, IT = getFromRestart(restart_file, TOPT, NX, NZ, StaticSolve)
+              
+              # Updates nolinear boundary condition to next Newton iteration
+              dWBC = SOLT[wbdex,0] - dHdX[hdex] * (INIT[ubdex] + SOLT[ubdex,0])  
+       else:
+              # Set the initial time
+              IT = 0.0
+              thisTime = IT
+              
+              # Initial change in vertical velocity at boundary
+              dWBC = -dHdX[hdex] * INIT[ubdex]
+            
+       # Prepare the current fields (TO EVALUATE CURRENT JACOBIAN)
+       fields, U, W = \
+              eqs.computePrepareFields(OPS, np.array(SOLT[:,0]), udex, wdex, pdex, tdex)
+       
        #%% RAYLEIGH AND GML WEIGHT OPERATORS
        ROPS, RLM, GML, LDEX = computeRayleighEquations(DIMS, REFS, ZRL, RLOPT)
        
@@ -689,7 +732,7 @@ def runModel(TestName):
        del(PORZ)
        del(DUDZ)
        del(DLTDZ)
-       del(DLPDZ)
+       #del(DLPDZ)
        del(DLPTDZ)
        del(GML)
        del(REF0)
@@ -700,25 +743,43 @@ def runModel(TestName):
        # Derivative operators from global spectral methods
        DDXMS1, DDZMS1 = devop.computePartialDerivativesXZ(DIMS, REFS[7], DDX_1D, DDZ_1D)
        
-       DDX_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[0], 6)
-       DDZ_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[1], 6)
-       DDX_CS, dummy = derv.computeCubicSplineDerivativeMatrix(REFS[0], False, True, DDX_CFD)
-       DDZ_CS, dummy = derv.computeCubicSplineDerivativeMatrix(REFS[1], False, True, DDZ_CFD)
+       #DDX_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[0], 6)
+       #DDZ_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[1], 6)
+       DDX_CS, dummy = derv.computeCubicSplineDerivativeMatrix(REFS[0], False, True, None)
+       DDZ_CS, dummy = derv.computeCubicSplineDerivativeMatrix(REFS[1], False, True, None)
        DDXMS_LO, DDZMS_LO = devop.computePartialDerivativesXZ(DIMS, REFS[7], DDX_CS, DDZ_CS)
        
-       DDX_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[0], 8)
-       DDZ_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[1], 8)
-       DDX_QS, dummy = derv.computeQuinticSplineDerivativeMatrix(REFS[0], False, True, DDX_CFD)
-       DDZ_QS, dummy = derv.computeQuinticSplineDerivativeMatrix(REFS[1], False, True, DDZ_CFD)
+       #DDX_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[0], 8)
+       #DDZ_CFD = derv.computeCompactFiniteDiffDerivativeMatrix1(REFS[1], 8)
+       DDX_QS, dummy = derv.computeQuinticSplineDerivativeMatrix(REFS[0], True, False, DDX_CS)
+       DDZ_QS, dummy = derv.computeQuinticSplineDerivativeMatrix(REFS[1], True, False, DDZ_CS)
        DDXMS_HO, DDZMS_HO = devop.computePartialDerivativesXZ(DIMS, REFS[7], DDX_QS, DDZ_QS)
        
        DZTM = sps.diags(np.reshape(DZT, (OPS,), order='F'))
+       
+       '''
+       
+       print(DDZ_CS[-1,:])
+       print(DDZ_QS[-1,:])
+       
+       fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+       ax.plot_surface(REFS[4], REFS[5], 
+                       np.reshape(DDZMS_LO @ hydroState[:,2], REFS[4].shape, order='F'), 
+                       cmap=cm.jet, linewidth=0, antialiased=False)
+       
+       fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+       ax.plot_surface(REFS[4], REFS[5], 
+                       np.reshape(DDZMS_HO @ hydroState[:,2], REFS[4].shape, order='F'), 
+                       cmap=cm.jet, linewidth=0, antialiased=False)
+       plt.show()
+       input('CHECK GRADIENTS...')
+       '''
                      
        #%% Prepare derivative operators for advection              
        if HermFunc:
               PPXMA = DDXMS1
        else:
-              PPXMA = DDXMS_HO# - DZTM @ DDZMS_HO
+              PPXMA = DDXMS_LO# - DZTM @ DDZMS_HO
               
        if verticalChebGrid or verticalLegdGrid:
               #advtOps = (REFG[0][1].dot(PPXMD),
@@ -727,7 +788,7 @@ def runModel(TestName):
        else:
               #advtOps = (REFG[0][1].dot(PPXMD),
               #           REFG[0][2].dot(DDZMS_HO))
-              advtOps = (PPXMA,DDZMS_HO)
+              advtOps = (PPXMA,DDZMS_LO)
        
        PPXMD = DDXMS_LO #- DZTM @ DDZMS_HO
        diffOps = (PPXMD,DDZMS_LO)
@@ -792,43 +853,6 @@ def runModel(TestName):
        del(dummy)
        
        #%% SOLUTION INITIALIZATION
-       physDOF = numVar * OPS
-       lmsDOF = len(ubdex)
-       
-       # Initialize solution storage
-       SOLT = np.zeros((physDOF, 2))
-       
-       # Initialize Lagrange Multiplier storage
-       LMS = np.zeros(lmsDOF)
-       
-       # Initialize hydrostatic background
-       INIT = np.zeros((physDOF,))
-       RHS = np.zeros((physDOF,))
-       
-       # Initialize the Background fields
-       INIT[udex] = np.reshape(UZ, (OPS,), order='F')
-       INIT[wdex] = np.zeros((OPS,))
-       INIT[pdex] = np.reshape(LOGP, (OPS,), order='F')
-       INIT[tdex] = np.reshape(LOGT, (OPS,), order='F')
-       hydroState = np.reshape(INIT, (OPS, numVar), order='F')
-       
-       if isRestart and StaticSolve:
-              print('Restarting from previous solution...')
-              SOLT, LMS, DCF, NX_in, NZ_in, IT = getFromRestart(restart_file, TOPT, NX, NZ, StaticSolve)
-              
-              # Updates nolinear boundary condition to next Newton iteration
-              dWBC = SOLT[wbdex,0] - dHdX[hdex] * (INIT[ubdex] + SOLT[ubdex,0])  
-       else:
-              # Set the initial time
-              IT = 0.0
-              thisTime = IT
-              
-              # Initial change in vertical velocity at boundary
-              dWBC = -dHdX[hdex] * INIT[ubdex]
-            
-       # Prepare the current fields (TO EVALUATE CURRENT JACOBIAN)
-       fields, U, W = \
-              eqs.computePrepareFields(OPS, np.array(SOLT[:,0]), udex, wdex, pdex, tdex)
               
        #% Compute the global LHS operator and RHS
        if StaticSolve:
@@ -1105,10 +1129,7 @@ def runModel(TestName):
 
               # Create a container for DynSGS scaling and region parameters
               DLD = (DL1, DL2, DL1**2, DL2**2, DTF * DLS, S, DA / DIMS[-1], regDex, regDms)
-              
-              #RLM_gpu = cp.asarray(REFG[4][0].data)
-              RLM = REFG[4][0].data
-              
+                            
               # Initialize residual coefficient storage
               CRES = np.zeros((fields.shape[0],2,1))
               
@@ -1395,8 +1416,8 @@ if __name__ == '__main__':
        #TestName = 'ClassicalScharIter'
        #TestName = 'UniformStratStatic'
        #TestName = 'DiscreteStratStatic'
-       #TestName = 'UniformTestTransient'
-       TestName = '3LayerTestTransient'
+       TestName = 'UniformTestTransient'
+       #TestName = '3LayerTestTransient'
        
        # Run the model in a loop if needed...
        for ii in range(1):

DerivativeTest.py
@@ -39,8 +39,8 @@ def function1(x):
        
        for ii in range(len(x)):
               if x[ii] >= 0.5:
-                     Y[ii] = 1.0
-                     DY[ii] = 0.0
+                     Y[ii] = 1.0 - 2.0*x[ii]
+                     DY[ii] = -2.0
 
        return Y, DY
 
@@ -49,7 +49,9 @@ def function2(x, L):
        A = 4.0
        B = 2.0
        C = 4.0
+       
        Y = C * np.exp(-A / L * x) * np.sin(B * mt.pi / L * x)
+       
        DY = -(A * C) / L * np.exp(-A / L * x) * np.sin(B * mt.pi / L * x)
        DY += (B * C) * mt.pi / L * np.exp(-A / L * x) * np.cos(B * mt.pi / L * x)
        
@@ -70,8 +72,8 @@ NX = NNX * NEX
 DIMS = [L1, L2, ZH, NX, NZ]
 DIMS_EL = [L1, L2, ZH, NNZ, NZ]
 # Define the computational and physical grids+
-REFS = computeGrid(DIMS, True, False, False, True)
-REFS_EL = computeGrid(DIMS_EL, True, False, False, True)
+REFS = computeGrid(DIMS, None, True, False, False, True)
+REFS_EL = computeGrid(DIMS_EL, None, True, False, False, True)
 
 #%% SCSE VS SPECTRAL TEST Z DIRECTION
 '''
@@ -181,8 +183,8 @@ NZ = 95
 DIMS = [L1, L2, ZH, NX, NZ]
 
 # Define the computational and physical grids+
-REFS_CH = computeGrid(DIMS, True, False, True, False)
-REFS_LG = computeGrid(DIMS, True, False, False, True)
+REFS_CH = computeGrid(DIMS, None, True, False, True, False)
+REFS_LG = computeGrid(DIMS, None, True, False, False, True)
 
 #% Compute the raw derivative matrix operators in alpha-xi computational space
 DDZ_LG, LG_TRANS = derv.computeLegendreDerivativeMatrix(DIMS)
@@ -204,8 +206,8 @@ print('Compact FD6: ', np.count_nonzero(DDZ_CFD2))
 print('Compact FD8: ', np.count_nonzero(DDZ_CFD3))
 
 # Take boundary information from compact FD operators
-DDZ_CS, DDZ2_CS = derv.computeCubicSplineDerivativeMatrix(zv, False, True, DDZ_CFD2)
-DDZ_QS, DDZ4_QS = derv.computeQuinticSplineDerivativeMatrix(zv, False, True, DDZ_CFD3)
+DDZ_CS, DDZ2_CS = derv.computeCubicSplineDerivativeMatrix(zv, False, True, None)
+DDZ_QS, DDZ4_QS = derv.computeQuinticSplineDerivativeMatrix(zv, False, True, DDZ_CS)
 
 # SUPER-RESOLUTION SPECTRAL DERIVATIVE
 DDZ_AV = 0.5 * (DDZ_CFD2 + DDZ_QS) 

computeDerivativeMatrix.py
@@ -83,7 +83,7 @@ def numericalCleanUp(DDM):
        # Clean up numerical zeros
        for ii in range(N[0]):
               for jj in range(N[1]):
-                     if abs(DDM[ii,jj]) <= ZTOL:
+                     if abs(DDM[ii,jj]) < ZTOL:
                             DDMC[ii,jj] = 0.0
        return DDMC
 
@@ -660,7 +660,7 @@ def computeQuinticSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
                      D[ii,ii-1:ii+3] += a0 * dom[ii]**2 * ET2[3,:] + dom[ii] * ET2[4,:] + ET2[5,:]
                      
                      # Compute the C matrix (coefficients to Z)
-                     C[0,0] = -a2 * hm**3
+                     C[0,0] = -a2 * hp**3
                      C[0,0] += a0 * dom[0]**2 * OM1[0,0] + dom[0] * OM1[1,0] + OM1[2,0]
                      
                      # Compute the D matrix (coefficients to Q)
@@ -697,7 +697,7 @@ def computeQuinticSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
                      D[ii,ii-2:ii+2] += a0 * dom[ii]**2 * ET1[3,:] + dom[ii] * ET1[4,:] + ET1[5,:]
                      
                      # Compute the C matrix (coefficients to Z)
-                     C[-1,-1] = a2 * hp**3
+                     C[-1,-1] = -a2 * hm**3
                      C[-1,-1] += a0 * dom[-1]**2 * OM2[0,-1] + dom[-1] * OM2[1,-1] + OM2[2,-1]
                      
                      # Compute the D matrix (coefficients to Z)
@@ -745,8 +745,8 @@ def computeQuinticSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
               A[0,1] = -1.0 * dom[0] / h0
               B[0,:] = np.zeros(N)
               
-              A[N-1,N-2] = +1.0 * dom[-1] / hn
-              A[N-1,N-1] = -1.0 * dom[-2] / hn
+              A[N-1,N-2] = -1.0 * dom[-1] / hn
+              A[N-1,N-1] = +1.0 * dom[-2] / hn
               B[N-1,:] = np.zeros(N)
               
               Q, R = scl.qr(A)
@@ -761,7 +761,7 @@ def computeQuinticSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
        DDM = C.dot(AIB) + D
 
        # Set boundary derivatives from specified
-       if isClamped or not isEssential:
+       if isClamped or isEssential:
               DDM[0,:] = D1A
               DDM[-1,:] = D1B
        #else:
@@ -778,17 +778,6 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
        B = np.zeros((N,N)) # coefficients to RHS of 2nd derivatives
        C = np.zeros((N,N)) # coefficients to 1st derivatives
        D = np.zeros((N,N)) # coefficients to additive part of 1st derivatives
-       
-       if isClamped and np.isscalar(DDM_BC):
-              if DDM_BC == 0.0:
-                     DDM_BC = np.zeros((2,N))
-              else:
-                     print('Setting derivatives to single values (not 0) at ends NOT SUPPORTED!')
-                     DDM_BC = np.zeros((2,N))
-              
-              DM2 = DDM_BC * DDM_BC
-       else:
-              DM2 = DDM_BC.dot(DDM_BC)
                      
        # Loop over each interior point in the irregular grid
        for ii in range(1,N-1):
@@ -810,22 +799,30 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
               C[ii,ii+1] = -1.0 / 6.0 * hp
               
               D[ii,ii] = -1.0 / hp
-              D[ii,ii+1] = 1.0 / hp
+              D[ii,ii+1] = +1.0 / hp
               
        h0 = abs(dom[1] - dom[0])
        hn = abs(dom[-1] - dom[-2])
        
-       # Prescribed derivative conditions
-       D2A = DM2[0,:] # left end 4th derivative
-       D2B = DM2[-1,:] # right end 4th derivative
-       D1A = DDM_BC[0,:] # left end 1st derivative
-       D1B = DDM_BC[-1,:] # right end 1st derivative
+       C[N-1,N-1] = -1.0 / 3.0 * hn
+       C[N-1,N-2] = -1.0 / 6.0 * hn
+       
+       D[N-1,N-1] = +1.0 / hn
+       D[N-1,N-2] = -1.0 / hn
        
        if isClamped:
+              
+              if np.isscalar(DDM_BC):
+                     DM2 = DDM_BC * DDM_BC
+                     B[0,:] = DM2
+                     B[-1,:] = DM2
+              else:
+                     DM2 = DDM_BC.dot(DDM_BC)
+                     B[0,:] = DM2[0,:]
+                     B[-1,:] = DM2[-1,:]
+              
               A[0,0] = 1.0
-              B[0,:] = D2A
               A[-1,-1] = 1.0
-              B[-1,:] = D2B
               
               Q, R = scl.qr(A)
               PLU = scl.lu_factor(R)
@@ -835,8 +832,8 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
               A[0,1] = -1.0 / h0
               B[0,:] = np.zeros(N)
               
-              A[N-1,N-2] = +1.0 / hn
-              A[N-1,N-1] = -1.0 / hn
+              A[N-1,N-2] = -1.0 / hn
+              A[N-1,N-1] = +1.0 / hn
               B[N-1,:] = np.zeros(N)
               
               Q, R = scl.qr(A)
@@ -851,9 +848,13 @@ def computeCubicSplineDerivativeMatrix(dom, isClamped, isEssential, DDM_BC):
        DDM = C.dot(AIB) + D
 
        # Set boundary derivatives from specified
-       if isClamped or not isEssential:
-              DDM[0,:] = D1A
-              DDM[-1,:] = D1B
+       if isClamped:
+              if np.isscalar(DDM_BC):
+                     DDM[0,:] = DDM_BC
+                     DDM[-1,:] = DDM_BC
+              else:
+                     DDM[0,:] = DDM_BC[0,:]
+                     DDM[-1,:] = DDM_BC[-1,:]
        #else:
               
        DDM = numericalCleanUp(DDM)

computeEulerEquationsLogPLogT.py
@@ -13,18 +13,19 @@ import sparse_dot_mkl as spk
 # Change floating point errors
 np.seterr(all='ignore', divide='raise', over='raise', invalid='raise')
 
-def enforceBC_RHS(rhs, ebcDex):
+def enforceBC_RHS(PHYS, rhs, ebcDex):
        
        ldex = ebcDex[0]
-       rdex = ebcDex[0]
+       rdex = ebcDex[1]
        bdex = ebcDex[2]
        tdex = ebcDex[3]
        
        # BC conditions on tendencies
        rhs[ldex,:] = 0.0
-       #rhs[rdex,1] = 0.0
+       rhs[rdex,2] = 0.0
        rhs[bdex,0:2] = 0.0
-       #rhs[tdex,0] = 0.0
+       rhs[bdex,3] = 0.0
+       rhs[tdex,2] = 0.0
        
        return rhs
 
@@ -70,12 +71,14 @@ def computeRdT(PHYS, sol, pert, RdT_bar):
        #T_exp = np.exp(earg, dtype=np.longdouble)                 
               
        earg = kap * sol[:,2] + sol[:,3] - kap * lp0
-       RdT = Rd * np.exp(earg, dtype=np.longdouble)
-       #RdT = RdT_bar * (T_ratio + 1.0)
+       RdT1 = Rd * np.exp(earg, dtype=np.longdouble)
+       RdT2 = RdT_bar * (T_ratio + 1.0)
        #RdT = RdT_bar * T_exp
        #T_ratio = T_exp - 1.0
+       
+       #print(RdT1.max(), RdT2.max())
                      
-       return RdT.astype(np.float64), T_ratio.astype(np.float64)
+       return RdT1.astype(np.float64), T_ratio.astype(np.float64)
        #return RdT, T_ratio
 
 def computeFieldDerivatives(q, DDX, DDZ, RSBops):
@@ -119,7 +122,7 @@ def computeRHS(fields, hydroState, DDX, DDZ, dhdx, PHYS, REFS, REFG, withRay, is
        pertb = Q - hydroState
        
        # Compute pressure gradient force scaling (buoyancy)
-       RdT, T_ratio = computeRdT(Q, pertb, REFS[9][0], PHYS)
+       RdT, T_ratio = computeRdT(PHYS, Q, pertb, REFS[9][0])
        
        # Compute the updated RHS
        PqPx, DqDz = computeFieldDerivatives(fields, DDX, DDZ, RSBops)
@@ -347,7 +350,7 @@ def computeEulerEquationsLogPLogT_Explicit(PHYS, PqPx, PqPz, DqDz,
        
        DqDt = computeAdvectionLogPLogT_Explicit(PHYS, PqPx, PqPz, state)
        
-       DqDt = computeInternalForceLogPLogT_Explicit(PHYS, PqPx, DqDz, RdT, T_ratio, DqDt)
+       DqDt = computeInternalForceLogPLogT_Explicit(PHYS, PqPx, PqPz, RdT, T_ratio, DqDt)
        
        return DqDt
 

computeGrid.py
@@ -27,15 +27,7 @@ def computeGrid(DIMS, RLOPT, Herm, Four, ChebCol, LegCol):
               xi, wcp = qxw.leglb(NZ) #[-1 +1]
               z = 0.5 * ZH * (1.0 + xi)
        else:
-              zu = np.linspace(0.0, ZH, num=NZ)
-              dz = abs(zu[1] - zu[0])
-              
-              zb = 0.5 * RLOPT[0]
-              NI = int(NZ * (ZH - zb) / ZH)
-              zi = np.linspace(0.0, ZH - zb, NI)
-              dz2 = 1.0 * dz
-              zt = np.linspace(ZH - zb + dz2, ZH, num=int(zb / dz2), endpoint=True)
-              z = np.concatenate((zi, zt))
+              z = np.linspace(0.0, ZH, num=NZ)
        
        # Map reference 1D domains to physical 1D domains
        if Herm and not Four:
@@ -44,17 +36,7 @@ def computeGrid(DIMS, RLOPT, Herm, Four, ChebCol, LegCol):
        elif Four and not Herm:
               x = np.linspace(L1, L2, num=NX, endpoint=True)
        else:
-              xu = np.linspace(L1, L2, num=NX, endpoint=True)
-              dx = abs(xu[1] - xu[0])
-              
-              # Interior grid with 2X coarser in sponge layers
-              xb = 0.5 * RLOPT[1]
-              NI = int(NX * ((L2 - L1) - 2.0 * xb) / (L2 - L1))
-              xi = np.linspace(L1 + xb, L2 - xb, NI)
-              dx2 = 1.0 * dx
-              xl = np.linspace(L1, L1 + xb, num=int(xb / dx2), endpoint=False)
-              xr = np.linspace(L2 - xb + dx2, L2, num=int(xb / dx2), endpoint=True)
-              x = np.concatenate((xl, xi, xr))
+              x = np.linspace(L1, L2, num=NX, endpoint=True)
        
        # Return the REFS structure
        REFS = [x, z]

computeTimeIntegration.py
@@ -89,11 +89,23 @@ def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, TOPT, \
               
               PqPxA = DqDxA - REFS[14] * PqPzA
               DqDzA = (PqPzA - DQDZ)
-                                   
+              '''
+              NX = REFS[4].shape[1]
+              NZ = REFS[5].shape[0]
+              #HDP = np.reshape(RdT * PqPzA[:,2] + PHYS[0], (NZ,NX), order='F')
+              HDP = np.reshape(PqPzA[:,2], (NZ,NX), order='F')
+              from matplotlib import cm
+              import matplotlib.pyplot as plt
+              fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+              ax.plot_surface(REFS[4], REFS[5], HDP, cmap=cm.jet,
+                              linewidth=0, antialiased=False)
+              plt.show()
+              input('CHECK GRADIENTS...')
+              '''                     
               # Compute local RHS
               rhsDyn = tendency.computeEulerEquationsLogPLogT_Explicit(PHYS, PqPxA, PqPzA, DqDzA, 
                                                                        RdT, T_ratio, solA)
-              rhsDyn = tendency.enforceBC_RHS(rhsDyn, ebcDex)
+              #rhsDyn = tendency.enforceBC_RHS(rhsDyn, ebcDex)
               
               if Residual_Update:
                      rhs = np.copy(rhsDyn)
@@ -151,10 +163,10 @@ def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, TOPT, \
               # Compute diffusive tendencies
               rhsDif = tendency.computeDiffusionTendency(P2qPx2, P2qPz2, P2qPzx, P2qPxz, \
                                                          ebcDex)
-              rhsDif = tendency.enforceBC_RHS(rhsDif, ebcDex)
               
               # Compute total RHS and apply BC
               rhs = (rhsDyn + rhsDif)
+              rhsDif = tendency.enforceBC_RHS(PHYS, rhs, ebcDex)
               
               # Apply stage update
               solB = sol2Update + coeff * DT * rhs
@@ -163,10 +175,10 @@ def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, TOPT, \
               RayD = np.reciprocal(1.0 + coeff * DT * mu * RLM)
               #RayDX = np.reciprocal(1.0 + coeff * DT * mu * RLMX)
               # Apply Rayleigh damping layer implicitly
-              solB[:,0] = RayD * solB[:,0] + (1.0 - RayD) * init0[:,0]
+              #solB[:,0] = RayD * solB[:,0] + (1.0 - RayD) * init0[:,0]
               solB[:,1] *= RayD
               solB[:,2] = RayD * solB[:,2] + (1.0 - RayD) * init0[:,2]
-              solB[:,3] = RayD * solB[:,3] + (1.0 - RayD) * init0[:,3]
+              #solB[:,3] = RayD * solB[:,3] + (1.0 - RayD) * init0[:,3]
               
               return solB
        