Many fixes. SVD filter on derivative matrices for least singular value.

2DMountainWavesNH.py
@@ -234,7 +234,7 @@ def displayResiduals(message, rhs, thisTime, DT, OPS):
        
        return err
 
-def initializeNetCDF(fname, thisTime, XL, ZTL, hydroState):
+def initializeNetCDF(fname, thisTime, XL, ZTL, hydroState, senseTemp):
        
        NX = XL.shape[1]
        NZ = ZTL.shape[0]
@@ -274,10 +274,12 @@ def initializeNetCDF(fname, thisTime, XL, ZTL, hydroState):
        zgvar[:,:,0] = ZTL
        # Create variables (background static fields)
        UVAR = m_fid.createVariable('U', 'f8', ('z', 'x', 'y'))
+       GVAR = m_fid.createVariable('TZ', 'f8', ('z', 'x', 'y'))
        PVAR = m_fid.createVariable('LNP', 'f8', ('z', 'x', 'y'))
        TVAR = m_fid.createVariable('LNT', 'f8', ('z', 'x', 'y'))
        # Store variables
        UVAR[:,:,0] = np.reshape(hydroState[:,0], (NZ,NX), order='F')
+       GVAR[:,:,0] = np.reshape(senseTemp, (NZ,NX), order='F')
        PVAR[:,:,0] = np.reshape(hydroState[:,2], (NZ,NX), order='F')
        TVAR[:,:,0] = np.reshape(hydroState[:,3], (NZ,NX), order='F')
        # Create variables (transient fields)
@@ -373,7 +375,7 @@ def runModel(TestName):
        if StaticSolve:
               RSBops = False
        else:
-              RSBops = False # Turn off PyRSB SpMV
+              RSBops = True # Turn off PyRSB SpMV
               if RSBops:
                      from rsb import rsb_matrix
        
@@ -488,7 +490,7 @@ def runModel(TestName):
        REF0.append(sig)
        
        tz, dtz = \
-              computeTemperatureProfileOnGrid(PHYS, REF0, Z_in, T_in, smooth3Layer, uniformStrat, RLOPT)
+              computeTemperatureProfileOnGrid(PHYS, REF0, Z_in, T_in, smooth3Layer, uniformStrat)
               
        dlpz, lpz, pz, dlptz, lpt, pt, rho = \
               computeThermoMassFields(PHYS, DIM0, REF0, tz, dtz, 'sensible', RLOPT)
@@ -760,7 +762,7 @@ def runModel(TestName):
        dWBC = fields[ebcDex[2],1] - dHdX * state[ebcDex[2],0]
               
        # Initialize output to NetCDF
-       newFname = initializeNetCDF(fname4Restart, thisTime, XL, ZTL, hydroState)
+       newFname = initializeNetCDF(fname4Restart, thisTime, XL, ZTL, hydroState, TZ)
               
        start = time.time()
        
@@ -1205,8 +1207,8 @@ if __name__ == '__main__':
        #TestName = 'ClassicalScharIter'
        #TestName = 'UniformStratStatic'
        #TestName = 'DiscreteStratStatic'
-       TestName = 'UniformTestTransient'
-       #TestName = '3LayerTestTransient'
+       #TestName = 'UniformTestTransient'
+       TestName = '3LayerTestTransient'
        
        # Run the model in a loop if needed...
        for ii in range(1):

DerivativeTest.py
@@ -72,8 +72,8 @@ NX = NNX * NEX
 DIMS = [L1, L2, ZH, NX, NZ]
 DIMS_EL = [L1, L2, ZH, NNZ, NZ]
 # Define the computational and physical grids+
-REFS = computeGrid(DIMS, None, True, False, False, True)
-REFS_EL = computeGrid(DIMS_EL, None, True, False, False, True)
+REFS = computeGrid(DIMS, True, False, False, True)
+REFS_EL = computeGrid(DIMS_EL, True, False, False, True)
 
 #%% SCSE VS SPECTRAL TEST Z DIRECTION
 '''
@@ -186,8 +186,8 @@ NZ = 95
 DIMS = [L1, L2, ZH, NX, NZ]
 
 # Define the computational and physical grids+
-REFS_CH = computeGrid(DIMS, None, True, False, True, False)
-REFS_LG = computeGrid(DIMS, None, True, False, False, True)
+REFS_CH = computeGrid(DIMS, True, False, True, False)
+REFS_LG = computeGrid(DIMS, True, False, False, True)
 
 #% Compute the raw derivative matrix operators in alpha-xi computational space
 DDZ_LG, LG_TRANS = derv.computeLegendreDerivativeMatrix(DIMS)
@@ -209,9 +209,9 @@ print('Compact FD6: ', np.count_nonzero(DDZ_CFD2))
 print('Compact FD8: ', np.count_nonzero(DDZ_CFD3))
 
 # Take boundary information from compact FD operators
-DDZ_CS, DDZ2_CS = derv.computeCubicSplineDerivativeMatrix(zv, False, True, DDZ_CFD1)
-DDZ_RS, DDZ3_RS = derv.computeQuarticSplineDerivativeMatrix(zv, False, True, DDZ_CFD1)
-DDZ_QS, DDZ4_QS = derv.computeQuinticSplineDerivativeMatrix(zv, False, True, DDZ_CFD1)
+DDZ_CS, DDZ2_CS = derv.computeCubicSplineDerivativeMatrix(zv, True, False, DDZ_CFD1)
+DDZ_RS, DDZ3_RS = derv.computeQuarticSplineDerivativeMatrix(zv, True, False, DDZ_CFD1)
+DDZ_QS, DDZ4_QS = derv.computeQuinticSplineDerivativeMatrix(zv, True, False, DDZ_CFD2)
 
 # Compute eigenspectra
 W1 = scl.eigvals(DDZ_CFD2)

computeTemperatureProfileOnGrid.py
@@ -8,7 +8,7 @@ Created on Sat Jul 20 12:58:27 2019
 import numpy as np
 import scipy.interpolate as spi
 
-def computeTemperatureProfileOnGrid(PHYS, REFS, Z_in, T_in, isSmooth, isUniform, RLOPT):
+def computeTemperatureProfileOnGrid(PHYS, REFS, Z_in, T_in, isSmooth, isUniform):
        
        # Get REFS data
        Z = REFS[1]
@@ -32,7 +32,7 @@ def computeTemperatureProfileOnGrid(PHYS, REFS, Z_in, T_in, isSmooth, isUniform,
                      
               # Loop over each column and evaluate interpolant
               TZ = T_int(Z)
-              DTDZ = T_int(Z, nu=1)
+              DTDZ = np.gradient(TZ, Z, edge_order=2)
                                                                                                    
               '''
               # Make profile dry adiabatic near the top boundary (unsupportive of waves)

computeTopographyOnGrid.py
@@ -53,15 +53,15 @@ def computeTopographyOnGrid(REFS, opt):
        #DDX_BC = derv.computeCompactFiniteDiffDerivativeMatrix1(xh, 4)
        #DDX_CS, DDX2A_CS = derv.computeCubicSplineDerivativeMatrix(xh, True, False, DDX_BC)
        
-       DDX_CS, DDX2A_BC = derv.computeCubicSplineDerivativeMatrix(xh, True, False, 0.0)
-       DDX_QS, DDX4A_QS = derv.computeQuinticSplineDerivativeMatrix(xh, True, False, DDX_CS)
+       DDX_CS, DDX2A_BC = derv.computeCubicSplineDerivativeMatrix(xh, False, True, 0.0)
+       DDX_QS, DDX4A_QS = derv.computeQuinticSplineDerivativeMatrix(xh, False, True, DDX_CS)
        
        # Evaluate the function with different options
        if profile == 1:
               # Kaiser bell curve
               ht = h0 * sin2Dom
               # Take the derivative
-              dhdx = DDX_QS @ ht
+              dhdx = DDX_CS @ ht
        elif profile == 2:
               # Schar mountain with analytical slope
               ht1 = h0 * np.exp(-1.0 / aC**2.0 * np.power(xh, 2.0))
@@ -79,7 +79,7 @@ def computeTopographyOnGrid(REFS, opt):
               ht2 = 1.0 + hs * np.power(np.cos(mt.pi / lC * xh), ps)
               ht = hf * h0 * sin2Dom * ht2
               # Take the derivative
-              dhdx = DDX_QS @ ht
+              dhdx = DDX_CS @ ht
        elif profile == 4:
               # General even power exponential times a polynomial series
               ht = np.zeros(len(xh))

transientPlot.py
@@ -5,38 +5,38 @@ Created on Wed Mar 31 08:25:38 2021
 
 @author: jeg
 """
-import os
-import time
-from PIL import Image, ImageFile
+from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import numpy as np
-#import bottleneck as bn
-#import scipy.linalg as scl
-#import scipy.ndimage as scm
-import matplotlib as mpl
-from matplotlib import cm
 import matplotlib.pyplot as plt
-import proplot as pplt
+import matplotlib.colors as colors
+import scipy.ndimage as scm
+#import proplot as pplt
 from netCDF4 import Dataset
 from joblib import Parallel, delayed
 
 plt.rcParams.update({'font.size': 16})
 
-NF = 1540
+NF = 1600
+TIME2STOP = 3.0
 m2k = 1.0E-3
 runPertb = False
 runSGS = False
 runPar = False
 imgname = '/media/jeguerra/FastDATA/linearMtnWavesSolver/animations/toanimate'
-fname = '3Layer_150mXZ.nc'
+fname = '3Layer_200mXZ_SVD-FILT.nc'
 m_fid = Dataset(fname, 'r', format="NETCDF4")
 
 times = m_fid.variables['time'][:NF]
+timesH = times / 3600.0
 X = m2k * m_fid.variables['Xlon'][:,:,0]
 Z = m2k * m_fid.variables['Zhgt'][:,:,0]
 
-zdex = np.nonzero(Z <= 21.0)
-xdex = np.nonzero((-30.0 <= X) & (X <= +50.0))
+zbound = 20.0
+xbound1 = -30.0
+xbound2 = +50.0
+zdex = np.nonzero(Z <= zbound)
+xdex = np.nonzero((xbound1 <= X) & (X <= xbound2))
 
 #U = m_fid.variables['U'][:,:,0]
 #LNP = m_fid.variables['LNP'][:,:,0]
@@ -48,25 +48,25 @@ LNT = m_fid.variables['LNT'][:,:,0]
 lnt = m_fid.variables['ln_t'][:NF,:,:,0]
 
 # Compute the total and perturbation potential temperature
-TH = np.exp(lnt)
+TH = np.exp(lnt + LNT)
 th = TH - np.exp(LNT)
 
+cmp2plot = 'gist_ncar_r'
 if runPertb:
        var2plot = th
-       cmp2plot = 'nipy_spectral'
-       out_name = 'PerturbationPT01.gif'
+       #cmp2plot = 'nipy_spectral'
 else:
        var2plot = TH
        #cmp2plot = sns.color_palette('Spectral_r', as_cmap=True)
        #cmp2plot = pplt.Colormap('vikO')
-       cmp2plot = pplt.Colormap(
-              'YlGnBu_r', 'YlOrRd', 'RdYlGn', 'PRGn_r',
-              ratios=(1,1,3,3), name='SciVisColorEven', save=True)
-       out_name = 'TotalPT01.gif'
-
+       #cmp2plot = pplt.Colormap(
+       #       'YlGnBu_r', 'YlOrRd', 'RdYlGn', 'PRGn_r',
+       #       ratios=(1,1,3,3), name='SciVisColorEven', save=True)
+       
 # Get the upper and lower bounds for TH
 clim1 = 300.0
 clim2 = 550.0
+cline = np.linspace(clim1, clim2, num=36)
 print('Plot bounds: ', clim1, clim2)
 
 # Initialize figure
@@ -79,19 +79,25 @@ def plotPertb(tt):
        th2plot = np.ma.getdata(var2plot[tt,:zdex[0].max(),:])
        
        # Median spatial filter
-       #th2plot = scm.median_filter(th2plot, size=(4,3))
+       th2plot = scm.median_filter(th2plot, size=(6,6))
        
        cflow = plt.contourf(X[:zdex[0].max(),:], 
                             Z[:zdex[0].max(),:], 
-                            th2plot, 512, cmap=cmp2plot, vmin=clim1, vmax=clim2)
+                            th2plot, 256, 
+                            cmap=cmp2plot, 
+                            norm=colors.PowerNorm(gamma=0.5),
+                            vmin=clim1, vmax=clim2)
+       
        plt.contour(X[:zdex[0].max(),:], 
                    Z[:zdex[0].max(),:], 
-                   th2plot, 36, colors='k', vmin=clim1, vmax=clim2)
+                   th2plot, cline, colors='k',
+                   norm=colors.PowerNorm(gamma=0.5),
+                   vmin=clim1, vmax=clim2)
        
        plt.fill_between(X[0,:], Z[0,:], color='black')
        plt.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True)
-       plt.xlim(-25.0, 50.0)
-       plt.ylim(0.0, 20.0)
+       plt.xlim(xbound1, xbound2)
+       plt.ylim(0.0, zbound)
        plt.xlabel('Distance (km)')
        plt.ylabel('Elevation (km)')
        plt.title('Total ' + r'$\theta$ (K)' + \
@@ -114,77 +120,16 @@ def plotPertb(tt):
        #image = Image.open(save_file)
                      
        # Delete stuff
-       print('Hour: {timeH:.2f}'.format(timeH = times[tt] / 3600.0))
+       print('Hour: {thisTime:.2f}'.format(thisTime = timesH[tt]))
        
-       return save_file
-       
-#%% Contour animation of the normalized SGS
-if runSGS:
-       imglist = []
-       for tt in range(len(times)):
-              
-              if tt == 0:
-                     dt = times[tt+1] - times[tt]
-              else:
-                     dt = times[tt] - times[tt-1]
-                     
-              if dt < 1.0E-10:
-                     continue
-              
-              # Compute the SGS
-              if tt == 0:
-                     q = np.zeros(th[tt,:,:].shape)
-                     norm = 1.0
-              elif tt == 1:
-                     q = 1.0 / dt * (th[tt,:,:] - th[tt-1,:,:]) - \
-                            0.5 * (dlnt[tt-1,:,:] + dlnt[tt+1,:,:])
-                     norm = 1.0 / np.amax(np.abs(q))
-              elif tt == len(times)-1:
-                     q = 1.0 / dt * (th[tt,:,:] - th[tt-1,:,:]) - dlnt[tt,:,:]
-                     norm = 1.0 / np.amax(np.abs(q))
-              else:
-                     q = 0.5 / dt * (3.0 * th[tt,:,:] - 4.0 * th[tt-1,:,:] + th[tt-2,:,:]) - \
-                            0.5 * (dlnt[tt-1,:,:] + dlnt[tt+1,:,:])
-                     norm = 1.0 / np.amax(np.abs(q))
-                     
-              qSGS = norm * q
-              
-              fig = plt.figure(figsize=(16.0, 8.0))
-              plt.grid(visible=None, which='major', axis='both', color='k', linestyle='--', linewidth=0.25)
-              
-              cc = plt.contourf(1.0E-3*X, 1.0E-3*Z, qSGS[:,:], 64, cmap=cm.seismic, vmin=-1.0, vmax=1.0)
-              
-              norm = mpl.colors.Normalize(vmin=-1.0, vmax=1.0)
-              plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cm.seismic), format='%.2e')
-              
-              plt.fill_between(m2k * X[0,:], m2k * Z[0,:], color='black')
-              plt.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True)
-              plt.xlim(-50.0, 50.0)
-              plt.ylim(0.0, 25.0)
-              plt.title('Normalized SGS: ' + r'$\theta$' + \
-                        ' Hour: {timeH:.2f}'.format(timeH = times[tt] / 3600.0))
-              plt.tight_layout()
-              # Save out the image
-              plt.savefig(imgname)
-              time.sleep(0.01)
-              
-              # Get the current image and add to gif list
-              image = Image.open(imgname)
-              imglist.append(image)
-                            
-              # Delete stuff
-              os.remove(imgname)
-              plt.close('all')
-              del(fig)
+       return save_file     
+
 #%% Contour animation of perturbation potential temperatures
-else:    
-       # Parallel processing
-       if runPar:
-              print('Attempt parallel processing...')
-              imglist = Parallel(n_jobs=8)(delayed(plotPertb)(tt) for tt in range(len(times)))
-       else:
-              print('Run serial processing...')
-              #imglist = [plotPertb(tt) for tt in range(len(times))]
-              imglist = [plotPertb(tt) for tt in range(NF)]
+if runPar:
+       print('Attempt parallel processing...')
+       imglist = Parallel(n_jobs=8)(delayed(plotPertb)(tt) for tt in range(len(times)))
+else:
+       print('Run serial processing...')
+       #imglist = [plotPertb(tt) for tt in range(len(times))]
+       imglist = [plotPertb(tt) for tt in range(NF) if timesH[tt] < TIME2STOP]
        
-#imglist[0].save(out_name,append_images=imglist[1:], save_all=True, optimize=True, duration=30, loop=0)