Refactor DynSGS, fix numerical clean-up derivatives

2DMountainWavesNH.py
@@ -36,6 +36,7 @@ import scipy.sparse.linalg as spl
 import matplotlib.path as pth
 from matplotlib import cm
 import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
 
 # Import from the local library of routines
 from computeGrid import computeGrid
@@ -1005,9 +1006,10 @@ def runModel(TestName):
               DA = np.reshape(np.abs(DXV * DZV), (OPS,), order='F')
               
               # DynSGS filter scale lengths
-              DL1 = 2.0 * DX_max
-              DL2 = 2.0 * DZ_max
+              DL1 = mt.pi * DX_max
+              DL2 = mt.pi * DZ_max
               DLR = mt.sqrt(DL1**2 + DL2**2)
+              #DLR = 0.5 * (DL1 + DL2)
               
               print('Diffusion regions dimensions (m): ', DL1, DL2)
               
@@ -1016,33 +1018,38 @@ def runModel(TestName):
                      node = XZV[nn,:]
                      
                      if isRectRegion:
-                            if node[0] == DIMS[0]:
-                                   verts = np.array([(node[0] + 2*DL1, node[1] - DL2), \
-                                            (node[0] + 2*DL1, node[1] + DL2), \
-                                            (node[0] - 0.0, node[1] + DL2), \
-                                            (node[0] - 0.0, node[1] - DL2)])
-                            elif node[0] == DIMS[1]:
-                                   verts = np.array([(node[0] + 0.0, node[1] - DL2), \
-                                            (node[0] + 0.0, node[1] + DL2), \
-                                            (node[0] - 2*DL1, node[1] + DL2), \
-                                            (node[0] - 2*DL1, node[1] - DL2)])
-                            elif np.any(ZTL[0,:] == node[1]):
-                                   verts = np.array([(node[0] + DL1, node[1] - 0.0), \
-                                            (node[0] + DL1, node[1] + 2*DL2), \
-                                            (node[0] - DL1, node[1] + 2*DL2), \
-                                            (node[0] - DL1, node[1] - 0.0)])
-                            elif node[1] == DIMS[2]:
-                                   verts = np.array([(node[0] + DL1, node[1] - 2*DL2), \
-                                            (node[0] + DL1, node[1] + 0.0), \
-                                            (node[0] - DL1, node[1] + 0.0), \
-                                            (node[0] - DL1, node[1] - 2*DL2)])
+                            '''
+                            if np.any(XL[:,0] == node[0]) or np.any(XL[:,-1] == node[0]):
+                                   DLO = 2*DL1
+                                   verts = np.array([(node[0] + DLO, node[1] - DL2), \
+                                            (node[0] + DLO, node[1] + DL2), \
+                                            (node[0] - DLO, node[1] + DL2), \
+                                            (node[0] - DLO, node[1] - DL2)])
+                                   
+                                   region = pth.Path(verts).contains_points(XZV)
+                                   
+                            elif np.any(ZTL[0,:] == node[1]) or np.any(ZTL[-1,:] == node[1]):
+                                   DLO = 2*DL2
+                                   verts = np.array([(node[0] + DL1, node[1] - DLO), \
+                                            (node[0] + DL1, node[1] + DLO), \
+                                            (node[0] - DL1, node[1] + DLO), \
+                                            (node[0] - DL1, node[1] - DLO)])
+                                   
+                                   region = pth.Path(verts).contains_points(XZV)
                             else:
                                    verts = np.array([(node[0] + DL1, node[1] - DL2), \
                                             (node[0] + DL1, node[1] + DL2), \
                                             (node[0] - DL1, node[1] + DL2), \
                                             (node[0] - DL1, node[1] - DL2)])
-                            rectangle = pth.Path(verts)
-                            region = rectangle.contains_points(XZV)
+                                   
+                                   region = pth.Path(verts).contains_points(XZV)
+                            '''
+                            verts = np.array([(node[0] + DL1, node[1] - DL2), \
+                                     (node[0] + DL1, node[1] + DL2), \
+                                     (node[0] - DL1, node[1] + DL2), \
+                                     (node[0] - DL1, node[1] - DL2)])
+                            
+                            region = pth.Path(verts).contains_points(XZV)
                      else:
                             circle = pth.Path.circle(center=(node[0],node[1]),
                                                      radius=DLR)
@@ -1052,8 +1059,8 @@ def runModel(TestName):
                      
                      return regDex
               
-              from joblib import Parallel, delayed
-              fltDex = Parallel(n_jobs=8)(delayed(searchRegions)(ii) \
+              ncores = os.cpu_count() - 2
+              fltDex = Parallel(n_jobs=ncores)(delayed(searchRegions)(ii) \
                                           for ii in np.arange(XZV.shape[0]))
               #fltDex = [searchRegions(ii) for ii in np.arange(XZV.shape[0])]
               
@@ -1122,7 +1129,7 @@ def runModel(TestName):
                      try:   
                             state, dfields, rhsVec, resVec, DCF, thisDt = tint.computeTimeIntegrationNL(PHYS, REFS, REFG, \
                                                                     DLD, thisDt, TOPT, state, hydroState, rhsVec, dfields, \
-                                                                    DCF.shape, ebcDex, RSBops, VWAV_ref, res_norm, isInitialStep)
+                                                                    DCF, ebcDex, RSBops, VWAV_ref, res_norm, isInitialStep)
                             
                      except Exception:
                             print('Transient step failed! Closing out to NC file. Time: ', thisTime)
@@ -1141,16 +1148,6 @@ def runModel(TestName):
                                    
                             # Store in the NC file
                             store2NC(newFname, thisTime, ff, numVar, ZTL, state, rhsVec, resVec, DCF)
-                            
-                            if thisTime > 60.0 and updateSGS and not isInitialStep:
-                                   # compute function average of initial fields
-                                   sol_avrg = DLD[-3] @ state
-                                   # compute state relative to average
-                                   rnorm = DLD[-3] @ np.abs(state - sol_avrg)
-                                   
-                                   print('Updated Residual Norms:')
-                                   res_norm = 1.0 / rnorm
-                                   print(res_norm)
                                                         
                             ff += 1
                             diagTime = 0.0

computeDerivativeMatrix.py
@@ -78,12 +78,14 @@ def computeAdjustedOperatorPeriodic(D2A):
 def numericalCleanUp(DDM):
        
        N = DDM.shape
-       ZTOL = 1.0E-14
-       DDMC = np.copy(DDM)
+       ZTOL = 2.0E-16
+       DDMC = np.zeros(DDM.shape)
        # Clean up numerical zeros
        for ii in range(N[0]):
               for jj in range(N[1]):
-                     if abs(DDM[ii,jj]) < ZTOL:
+                     if abs(DDM[ii,jj]) >= ZTOL:
+                            DDMC[ii,jj] = DDM[ii,jj]
+                     else:
                             DDMC[ii,jj] = 0.0
        return DDMC
 

computeTimeIntegration.py
@@ -39,7 +39,7 @@ def plotRHS(x, rhs, ebcDex, label):
        return
        
 def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, DT, TOPT, \
-                             sol0, init0, rhs0, dsol0, CRES_SIZE, ebcDex, \
+                             sol0, init0, rhs0, dsol0, dcf0, ebcDex, \
                              RSBops, VWAV_ref, res_norm, isInitialStep):
        
        CRES = 0.0
@@ -69,7 +69,7 @@ def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, DT, TOPT, \
        
        def computeUpdate(coeff, solA, sol2Update):
               
-              nonlocal Auxilary_Updates,rhsDyn,rhsDif,CRES,DT
+              nonlocal Auxilary_Updates,rhsDyn,rhsDif,res_norm,CRES,DT
               
               # Compute time dependent state about initial conditions
               pertbA = solA - init0
@@ -87,25 +87,43 @@ def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, DT, TOPT, \
               # Compute local RHS
               rhsDyn = tendency.computeEulerEquationsLogPLogT_Explicit(PHYS, PqPxA, PqPzA, DqDzA, 
                                                                        RdT, T_ratio, solA)
-              
               if Auxilary_Updates:
+                     '''
+                     if not isInitialStep:
+                            # compute function average of initial fields
+                            sol_avrg = DLD[-3] @ solA
+                            # compute state relative to average
+                            rnorm = DLD[-3] @ np.abs(solA - sol_avrg)
+                            res_norm += 1.0 / rnorm
+                            res_norm *= 0.5
+                     '''
                      
-                     DT, VWAV_fld, VFLW_adv = tendency.computeNewTimeStep(PHYS, RdT, solA,
-                                                                          DLD, isInitial=isInitialStep)
-                     
-                     sbnd = 0.5 * DT * VWAV_ref**2                     
+                     # Compute residual estimate
+                     sbnd = 0.5 * DT * VWAV_ref**2
+                     #res = 0.5 * (rhs0 + rhsDyn)
                      res = 0.5 * ((rhsDyn - rhs0) + (dsol0 / DT - 0.5 * (rhs0 + rhsDyn)))
                      
-                     # Define residual as the timestep change in the RHS
-                     CRES = rescf.computeResidualViscCoeffs(res_norm, np.abs(res), DLD, DT, 
-                                                            bdex, sbnd, np.zeros(CRES_SIZE))
+                     # Compute new DynSGS coefficients
+                     CRES = rescf.computeResidualViscCoeffs(res_norm, 
+                                                            np.abs(res), 
+                                                            DLD, DT, 
+                                                            bdex, sbnd)
                                                             
                      # Residual contribution vanishes in the sponge layers
                      CRES[:,:,0] *= (1.0 - RLMA)
                      # Add in smooth diffusion field in the sponge layers
                      CRES[:,:,0] += sbnd * RLMA
+                     
+                     # Time average with previous coefficient field
+                     dcf1 = 0.5 * (CRES + dcf0)
+                     
+                     # Compute the new incoming time step
+                     DT, VWAV_fld, VFLW_adv = tendency.computeNewTimeStep(PHYS, RdT, solA,
+                                                                          DLD, isInitial=isInitialStep)
        
                      Auxilary_Updates = False
+              else:
+                     dcf1 = 0.5 * (CRES + dcf0)
               
               #%% Compute diffusive update
 
@@ -114,8 +132,8 @@ def computeTimeIntegrationNL(PHYS, REFS, REFG, DLD, DT, TOPT, \
               PqPzA = np.copy(DqDzA)
               
               # Compute diffusive fluxes
-              PqPxA *= CRES[:,0,:]
-              PqPzA *= CRES[:,1,:]
+              PqPxA *= dcf1[:,0,:]
+              PqPzA *= dcf1[:,1,:]
               
               # Compute derivatives of diffusive flux
               Dq = np.column_stack((PqPxA,PqPzA))